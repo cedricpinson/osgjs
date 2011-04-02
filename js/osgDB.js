09b357f34de3a93fce437e64962f8fee53ee9540
Cedric Pinson <cedric.pinson@plopbyte.net>
2011-04-02 16:40:43 +0200

    add osgDB.js file, we should put futur io code in this file

---
+++ js/osgDB.js
@@ -0,0 +1,22 @@
+/** -*- compile-command: "jslint-cli osgDB.js" -*-
+ *
+ *  Copyright (C) 2010 Cedric Pinson
+ *
+ *                  GNU LESSER GENERAL PUBLIC LICENSE
+ *                      Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ *
+ * This version of the GNU Lesser General Public License incorporates
+ * the terms and conditions of version 3 of the GNU General Public
+ * License
+ *
+ * Authors:
+ *  Cedric Pinson <cedric.pinson@plopbyte.net>
+ *
+ */
+
+var osgDB = {};
+osgDB.readNode = osg.ParseSceneGraph;
