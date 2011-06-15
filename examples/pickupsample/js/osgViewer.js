42d36c9fdaf83bdd697b2c8ec07ae8db5cc5a0ed
Akim Juillerat <akim.juillerat@heig-vd.ch>
2011-06-16 00:48:26 +0200

    pickupsample

---
+++ examples/pickupsample/js/osgViewer.js
@@ -0,0 +1,456 @@
+/** -*- compile-command: "jslint-cli osgViewer.js" -*-
+ *
+ *  Copyright (C) 2010 Cedric Pinson
+ *
+ *                  GNU LESSER GENERAL PUBLIC LICENSE
+ *                      Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ *
+ * This version of the GNU Lesser General Public License incorporates
+ * the terms and conditions of version 3 of the GNU General Public
+ * License
+ *
+ * Authors:
+ *  Cedric Pinson <cedric.pinson@plopbyte.com>
+ *
+ */
+
+var osgViewer = {};
+
+osgViewer.Viewer = function(canvas, options) {
+    if (options === undefined) {
+        options = {antialias : true};
+    }
+
+    gl = WebGLUtils.setupWebGL(canvas, options );
+    if (gl) {
+        this.gl = gl;
+        osg.init();
+        this.canvas = canvas;
+        this.frameRate = 60.0;
+        osgUtil.UpdateVisitor = osg.UpdateVisitor;
+        osgUtil.CullVisitor = osg.CullVisitor;
+        this.urlOptions = true;
+
+        this.mouseWheelEventNode = canvas;
+        this.eventNode = document;
+        if (options && options.mouseWheelEventNode) {
+            this.mouseWheelEventNode = options.mouseWheelEventNode;
+        }
+
+    } else {
+        throw "No WebGL implementation found";
+    }
+};
+
+
+osgViewer.Viewer.prototype = {
+    getScene: function() { return this.scene; },
+    setScene: function(scene) {
+        this.root.removeChildren();
+        this.root.addChild( scene );
+        this.scene = scene;
+    },
+
+    init: function() {
+        this.root = new osg.Node();
+        this.state = new osg.State();
+        this.view = new osg.View();
+        this.view.addChild(this.root);
+
+        var ratio = this.canvas.width/this.canvas.height;
+        this.view.setViewport(new osg.Viewport(0,0, this.canvas.width, this.canvas.height));
+        this.view.setViewMatrix(osg.Matrix.makeLookAt([0,0,-10], [0,0,0], [0,1,0]));
+        this.view.setProjectionMatrix(osg.Matrix.makePerspective(60, ratio, 1.0, 1000.0));
+
+        this.view.light = new osg.Light();
+        this.view.getOrCreateStateSet().setAttributeAndMode(new osg.Material());
+
+        gl.enable(gl.DEPTH_TEST);
+        gl.enable(gl.BLEND);
+        gl.enable(gl.CULL_FACE);
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
+
+        this.updateVisitor = new osgUtil.UpdateVisitor();
+        this.cullVisitor = new osgUtil.CullVisitor();
+
+        this.renderStage = new osg.RenderStage();
+        this.stateGraph = new osg.StateGraph();
+        this.renderStage.setViewport(this.view.getViewport());
+
+        if (this.urlOptions) {
+            this.parseOptions();
+        }
+    },
+
+    parseOptions: function() {
+
+        var optionsURL = function() {
+            var vars = [], hash;
+            var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+            for(var i = 0; i < hashes.length; i++)
+            {
+                hash = hashes[i].split('=');
+                vars.push(hash[0]);
+                vars[hash[0]] = hash[1];
+            }
+            return vars;
+        };
+        
+        var options = optionsURL();
+
+        if (options['stats'] === "1" || options['STATS'] === "1" || options['Stats'] === "1" ) {
+            this.initStats(options);
+        }
+
+        // not the best way to do it
+        if (options['DEPTH_TEST'] === "0") {
+            gl.disable(gl.DEPTH_TEST);
+        }
+        if (options['BLEND'] === "0") {
+            gl.disable(gl.BLEND);
+        }
+        if (options['CULL_FACE'] === "0") {
+            gl.disable(gl.CULL_FACE);
+        }
+        if (options['LIGHT'] === "0") {
+            delete this.view.light;
+        }
+
+         
+    },
+
+    initStats: function(options) {
+
+        var maxMS = 50;
+        var stepMS = 10;
+        var fontsize = 14;
+
+        if (options['statsMaxMS'] !== undefined) {
+            maxMS = parseInt(options['statsMaxMS']);
+        }
+        if (options['statsStepMS'] !== undefined) {
+            stepMS = parseInt(options['statsStepMS']);
+        }
+
+        var createDomElements = function (elementToAppend) {
+            var dom = [
+                "<div id='StatsDiv' style='float: left; position: relative; width: 300px; height: 150; z-index: 10;'>",
+                "<div id='StatsLegends' style='position: absolute; left: 0px; font-size: " + fontsize +"px;color: #ffffff;'>",
+
+                "<div id='frameRate' style='color: #00ff00;' > frameRate </div>",
+                "<div id='frameTime' style='color: #ffff00;' > frameTime </div>",
+                "<div id='updateTime' style='color: #d07b1f;'> updateTime </div>",
+                "<div id='cullTime' style='color: #73e0ff;'> cullTime </div>",
+                "<div id='drawTime' style='color: #ff0000;'> drawTime </div>",
+                "<div id='fps'> </div>",
+                
+                "</div>",
+
+                "<div id='StatsCanvasDiv' style='position: relative;'>",
+                "<canvas id='StatsCanvasGrid' width='300' height='150' style='z-index:-1; position: absolute; background: rgba(14,14,14,0.8); ' ></canvas>",
+                "<canvas id='StatsCanvas' width='300' height='150' style='z-index:8; position: absolute;' ></canvas>",
+                "<canvas id='StatsCanvasFps' width='30' height='15' style='z-index:9; position: absolute; top: 130px' ></canvas>",
+                "</div>",
+
+                "</div>"
+            ].join("\n");
+            if (elementToAppend === undefined) {
+                elementToAppend = "body";
+            }
+            jQuery(dom).appendTo(elementToAppend);
+            var grid = document.getElementById("StatsCanvasGrid");
+            var ctx = grid.getContext("2d");
+            ctx.clearRect(0,0,grid.width, grid.height);
+
+            var step = Math.floor(maxMS/stepMS).toFixed(0);
+            var r = grid.height/step;
+            ctx.strokeStyle = "rgb(70,70,70)";
+            for (var i = 0, l = step; i < l; i++) {
+                ctx.beginPath();
+                ctx.moveTo(0, i*r);
+                ctx.lineTo(grid.width, i*r);
+                ctx.stroke();
+            }
+
+            // setup the font for fps
+            var cfps = document.getElementById("StatsCanvasFps");
+            var ctx = cfps.getContext("2d");
+            ctx.font = "14px Sans";
+
+            return document.getElementById("StatsCanvas");
+        };
+
+        if (this.canvasStats === undefined || this.canvasStats === null) {
+            this.canvasStats = createDomElements();
+        }
+        this.stats = new Stats.Stats(this.canvasStats);
+        var that = this;
+        this.frameRate = 1;
+        this.frameTime = 0;
+        this.updateTime = 0;
+        this.cullTime = 0;
+        this.drawTime = 0;
+        var height = this.canvasStats.height;
+        var ratio = height / maxMS;
+        height = height - 2;
+        this.stats.addLayer(jQuery("#frameRate").css("color"), function(t) { 
+            var v = (height)/60.0 * (1000/that.frameRate);
+            if (v > height) {
+                return height;
+            }
+            return v;} );
+        this.stats.addLayer(jQuery("#frameTime").css("color"), function(t) { 
+            var v = that.frameTime * ratio;
+            if (v > height) {
+                return height;
+            }
+            return v;} );
+        this.stats.addLayer(jQuery("#updateTime").css("color"), function(t) { 
+            var v = that.updateTime * ratio;
+            if (v > height) {
+                return height;
+            }
+            return v;} );
+        this.stats.addLayer(jQuery("#cullTime").css("color"), function(t) { 
+            var v = that.cullTime * ratio;
+            if (v > height) {
+                return height;
+            }
+            return v;} );
+        this.stats.addLayer(jQuery("#drawTime").css("color"), function(t) { 
+            var v = that.drawTime * ratio;
+            if (v > height) {
+                return height;
+            }
+            return v;} );
+    },
+
+    update: function() {
+        this.view.accept(this.updateVisitor);
+    },
+    cull: function() {
+        this.stateGraph.clean();
+        this.renderStage.reset();
+
+        this.cullVisitor.reset();
+        this.cullVisitor.setStateGraph(this.stateGraph);
+        this.cullVisitor.setRenderStage(this.renderStage);
+
+        //this.renderStage.setViewport(this.view.getClearDepth());
+        this.renderStage.setClearDepth(this.view.getClearDepth());
+        this.renderStage.setClearColor(this.view.getClearColor());
+        this.renderStage.setClearMask(this.view.getClearMask());
+
+        this.view.accept(this.cullVisitor);
+    },
+    draw: function() {
+        this.renderStage.draw(this.state);
+
+        // noticed that we accumulate lot of stack, maybe because of the stateGraph
+        this.state.popAllStateSets();
+        // should not be necessary because of dirty flag now in attrubutes
+        //this.state.applyWithoutProgram();
+    },
+
+    frame: function() {
+        var frameTime, beginFrameTime;
+        frameTime = (new Date()).getTime();
+        if (this.lastFrameTime === undefined) {
+            this.lastFrameTime = 0;
+        }
+        this.frameRate = frameTime - this.lastFrameTime;
+        this.lastFrameTime = frameTime;
+        beginFrameTime = frameTime;
+
+        if (this.updateVisitor.getFrameStamp().getFrameNumber() === 0) {
+            this.updateVisitor.getFrameStamp().setReferenceTime(frameTime/1000.0);
+            this.numberFrame = 0;
+        }
+
+        this.updateVisitor.getFrameStamp().setSimulationTime(frameTime/1000.0 - this.updateVisitor.getFrameStamp().getReferenceTime());
+
+        if (this.manipulator) {
+            this.view.setViewMatrix(this.manipulator.getInverseMatrix());
+        }
+
+        // time the update
+        var updateTime = (new Date()).getTime();
+        this.update();
+
+        var cullTime = (new Date()).getTime();
+        updateTime = cullTime - updateTime;
+        this.updateTime = updateTime;
+
+        this.cull();
+        var drawTime = (new Date()).getTime();
+        cullTime = drawTime - cullTime;
+        this.cullTime = cullTime;
+
+        this.draw();
+        drawTime = (new Date()).getTime() - drawTime;
+        this.drawTime = drawTime;
+
+        var f = this.updateVisitor.getFrameStamp().getFrameNumber()+1;
+        this.updateVisitor.getFrameStamp().setFrameNumber(f);
+
+        this.numberFrame++;
+        var endFrameTime = (new Date()).getTime();
+
+        this.frameTime = (new Date()).getTime() - beginFrameTime;
+        if (this.stats !== undefined) {
+            this.stats.update();
+
+            if (this.numberFrame % 60 === 0.0) {
+                var nd = endFrameTime;
+                var diff = nd - this.statsStartTime;
+                var fps = (this.numberFrame*1000/diff).toFixed(1);
+                this.statsStartTime = nd;
+                this.numberFrame = 0;
+
+                var cfps = document.getElementById("StatsCanvasFps");
+                var ctx = cfps.getContext("2d");
+                ctx.clearRect(0,0,cfps.width, cfps.height);
+                ctx.fillStyle = "rgb(255,255,255)";
+                ctx.fillText(fps, 0, cfps.height);
+            }
+        }
+    },
+
+    run: function() {
+        var that = this;
+        var render = function() {
+            window.requestAnimationFrame(render, this.canvas);
+            that.frame();
+        };
+        render();
+    },
+
+    getManipulator: function() { return this.manipulator; },
+    setupManipulator: function(manipulator, dontBindDefaultEvent) {
+        if (manipulator === undefined) {
+            manipulator = new osgGA.OrbitManipulator();
+        }
+
+        if (manipulator.setNode !== undefined) {
+            manipulator.setNode(this.root);
+        } else {
+            // for backward compatibility
+            manipulator.view = this.view;
+        }
+
+        this.manipulator = manipulator;
+
+        var that = this;
+        this.manipulator.convertEventToCanvas = function(e) {
+            var myObject = that.canvas;
+            var posx,posy;
+	    if (e.pageX || e.pageY) {
+	        posx = e.pageX;
+	        posy = e.pageY;
+	    }
+	    else if (e.clientX || e.clientY) {
+	        posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+	        posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+	    }
+
+            var divGlobalOffset = function(obj) {
+                var x=0, y=0;
+                x = obj.offsetLeft;
+                y = obj.offsetTop;
+                var body = document.getElementsByTagName('body')[0];
+                while (obj.offsetParent && obj!=body){
+                    x += obj.offsetParent.offsetLeft;
+                    y += obj.offsetParent.offsetTop;
+                    obj = obj.offsetParent;
+                }
+                return [x,y];
+            };
+	    // posx and posy contain the mouse position relative to the document
+	    // Do something with this information
+            var globalOffset = divGlobalOffset(myObject);
+            posx = posx - globalOffset[0];
+            posy = myObject.height-(posy - globalOffset[1]);
+            return [posx,posy];
+        };
+
+        if (dontBindDefaultEvent === undefined || dontBindDefaultEvent === false) {
+
+            var disableMouse = false;
+
+            var touchDown = function(ev)
+            {
+                disableMouse = true;
+                return Viewer.getManipulator().touchDown(ev);
+            };
+            var touchUp = function(ev)
+            {
+                disableMouse = true;
+                return Viewer.getManipulator().touchUp(ev);
+            };
+            var touchMove = function(ev)
+            {
+                disableMouse = true;
+                return Viewer.getManipulator().touchMove(ev);
+            };
+
+            this.canvas.addEventListener("MozTouchDown", touchDown, false);
+            this.canvas.addEventListener("MozTouchUp", touchUp, false);
+            this.canvas.addEventListener("MozTouchMove", touchMove, false);
+
+            jQuery(this.eventNode).bind( {
+                mousedown: function(ev) {
+                    if (disableMouse === false) {
+                        return manipulator.mousedown(ev);
+                    }
+                },
+                mouseup: function(ev) {
+                    if (disableMouse === false) {
+                        return manipulator.mouseup(ev);
+                    }
+                },
+                mousemove: function(ev) {
+                    if (disableMouse === false) {
+                        return manipulator.mousemove(ev);
+                    }
+                },
+                dblclick: function(ev) {
+                    if (disableMouse === false) {
+                        return manipulator.dblclick(ev);
+                    }
+                }
+            });
+
+            if (jQuery(document).mousewheel !== undefined) {
+                jQuery(this.canvas).mousewheel(function(objEvent, intDelta, deltaX, deltaY) {
+	            if (intDelta > 0){
+                        if (manipulator.distanceDecrease) {
+                            manipulator.distanceDecrease();
+                        }
+	            }
+	            else if (intDelta < 0){
+                        if (manipulator.distanceIncrease) {
+                            manipulator.distanceIncrease();
+                        }
+	            }
+                    return false;
+	        });
+            }
+            
+            if (manipulator.keydown !== undefined) {
+                jQuery(this.eventNode).bind({'keydown': function(event) {
+                    return manipulator.keydown(event);
+                }});
+            }
+            if (manipulator.keyup !== undefined) {
+                jQuery(this.eventNode).bind({'keyup': function(event) {
+                    return manipulator.keyup(event);
+                }});
+            }
+        }
+    }
+};
---
+++ examples/pickupsample/js/pickup.js
@@ -0,0 +1,385 @@
+$.noConflict();
+var canvas, viewer, manipulatorOn, disk, sideAup, cover, pickupModel, pickup, inside, turn, camera, playing, audio, speedRadPCs, arm, armNode, mouseDown, yDepl;
+var pi = Math.PI;
+var xDown, lastX;
+var clientX, clientY, dx, dy, pushHit, onCell;
+
+jQuery("document").ready(function() {
+    console.log("doc ready");
+    
+    initCanvas();
+    setupNavMenu();
+    pickupModel = getMk2();
+    
+    
+    //setTimeout("initViewer()", 5000);
+    //initViewer();
+    
+});
+    
+function setupNavMenu(){
+    var action;
+    jQuery("a").click(function(){
+        action = jQuery(this).attr('href').substr(1);
+        switch(action){
+            case 'diskInOut':
+                moveInOutDisk();
+                break;
+            case 'showPickup':
+                showPickup();
+                break;
+            case 'startStop':
+                startStop();
+                break;
+            case 'changeFace':
+                changeFace();
+                break;     
+        }
+    });
+    
+}
+function showPickup(){
+    
+        createPickup();
+    
+}
+
+function startStop(){
+    if(!inside && pickup !== undefined){
+        if(!playing){
+            viewer.setupManipulator(undefined, true);
+            viewer.getManipulator().computeHomePosition();
+            viewer.getManipulator().update(0, 1);
+            setupMouseListener();
+            
+            var platineUpdateCallback = new RotatingUpdateCallback(getNodeFromName(camera, "platine"), 33, "yaw");
+                        
+            //                        platineUpdateCallback.setEndFunction(function(){
+            //                            if(this.lastX > 100){
+            //                                return true;
+            //                            }
+            //                            return false;
+            //                        }, pickup);
+                        
+            pickup.setUpdateCallback(platineUpdateCallback);
+                    
+            var diskUpdateCallback;
+                    
+            // si le disque est posé coté A ou B changer le sens de rotation
+            if(sideAup){
+                diskUpdateCallback = new RotatingUpdateCallback(getNodeFromName(disk, "db"), 33, "yaw");
+            } else {
+                diskUpdateCallback = new RotatingUpdateCallback(getNodeFromName(disk, "db"), 33, "yaw", false);
+            }
+                        
+            disk.setUpdateCallback(diskUpdateCallback);
+                    
+            playing = true;
+        } else {
+                        
+            pickup.setUpdateCallback(undefined);
+            disk.setUpdateCallback(undefined);
+            
+            viewer.setupManipulator();
+            viewer.getManipulator().computeHomePosition();
+                    
+            playing = false;
+        }
+    
+//        if(manipulatorOn){
+//            
+//        } else {
+//            
+//        }
+        manipulatorOn = !manipulatorOn;
+    }
+}
+
+function changeFace(){
+    if(!inside && !playing && pickup !== undefined){
+        turn = 0;
+        yDepl = 13;
+        moveUpDisk();
+        sideAup = !sideAup;
+    }
+}
+
+function setupMouseListener(){
+    console.log("setup mouse");
+    jQuery("canvas").mouseup(function(event){
+        if(onCell && playing && mouseDown){
+            moveArmDown();
+        }
+        onCell = false;
+        mouseDown = false;
+        
+    })
+    jQuery("canvas").mousedown(function(event){
+        mouseDown = true;
+        lastX = event.clientX;
+        var pos = viewer.getManipulator().convertEventToCanvas(event);
+        clientX = pos[0];
+        clientY = pos[1];
+        console.log("X : " + clientX + " Y : " + clientY);
+        var x2 = clientX - jQuery("canvas").width()/2;
+        var y2 = clientY - jQuery("canvas").height()/2;
+        
+        
+        console.log("X2 : " + x2 + " y2 : " + y2);
+        
+        this.dx = this.dy = 0;
+        cellule = getNodeFromName(armNode, "g31");
+        var objX = cellule.boundingSphere._center[0];
+        var objY = cellule.boundingSphere._center[1];
+        var objR = cellule.boundingSphere._radius;
+        if ((x2-objX)*(x2-objX)+(y2-objY)*(y2-objY) <= (objR*objR) && playing){
+            console.log("collision");
+            onCell = true;
+            moveArmUp();
+        } else {
+            console.log("no collision");
+        }
+        
+        
+    //var hit = getIntersection();
+    //pushHit = hit;
+    //        lastX = xDown;
+    //        console.log("click");
+        
+    });
+    jQuery("canvas").mousemove(function(event){
+        if(mouseDown && onCell && playing){
+            
+            var clientX = event.clientX;
+            var deltaX = lastX-clientX;
+            //var xGap = xDown-clientX;
+            
+            //if(xGap < 150 && xGap > -150){
+            //            moveArm(deltax);
+            //}
+            moveArm(deltaX);
+            lastX = event.clientX;
+        }
+    });
+    
+}
+
+function getIntersection(){
+    
+    var hits = viewer.view.computeIntersections(clientX, clientY, 1);
+        
+    console.log("hits " + hits.length);
+        
+    var l = hits.length;
+    if (l === 0 ) {
+        return undefined;
+    }
+    hits.sort(function(a,b) {
+        return a.ratio - b.ratio;
+    });
+
+    // use the first hit
+    var hit = hits[0].nodepath;
+    var l2 = hit.length;
+    var itemSelected;
+    var name;
+    while (l2-- >= 0) {
+        if (hit[l2].itemToIntersect !== undefined) {
+            name = hit[l2].name;
+            //itemSelected = hit[l2].children[0].getUpdateCallback();
+            itemSelected = hit[l2];
+            break;
+        }
+    }
+    return {
+        'itemName': name, 
+        'item': itemSelected
+    };
+    
+}
+
+function moveArmUp(){
+    console.log("moveArmUp");
+    var currentPosition = arm.getMatrix();
+    
+    var copy = osg.Matrix.copy(currentPosition, copy);
+    var translation = osg.Matrix.makeTranslate(206.815448,98.028395,22.310888,[]);
+    var rotation = osg.Matrix.makeRotate(0.1, 1, 0, 0);
+    var backtranslation = osg.Matrix.inverse(translation, backtranslation);
+    
+    osg.Matrix.postMult(backtranslation, copy);
+    osg.Matrix.postMult(rotation, copy);
+    osg.Matrix.postMult(translation, copy);
+    
+    arm.setMatrix(copy);
+
+    armNode.removeChildren();
+    armNode.addChild(arm);
+}
+
+function moveArmDown(){
+    console.log("moveArmDown");
+    var currentPosition = arm.getMatrix();
+    
+    var copy = osg.Matrix.copy(currentPosition, copy);
+    var translation = osg.Matrix.makeTranslate(206.815448,98.028395,22.310888,[]);
+    var rotation = osg.Matrix.makeRotate(-0.1, 1, 0, 0);
+    var backtranslation = osg.Matrix.inverse(translation, backtranslation);
+    
+    osg.Matrix.postMult(backtranslation, copy);
+    osg.Matrix.postMult(rotation, copy);
+    osg.Matrix.postMult(translation, copy);
+    
+    arm.setMatrix(copy);
+
+    armNode.removeChildren();
+    armNode.addChild(arm);
+}
+
+
+function moveArm(deltax){
+    
+    console.log("moveArm : " + deltax);
+    
+    var currentPosition = arm.getMatrix();
+    
+    var copy = osg.Matrix.copy(currentPosition, copy);
+    var translation = osg.Matrix.makeTranslate(206.815448,98.028395,22.310888,[]);
+    var rotation = osg.Matrix.makeRotate(deltax/500, 0, 0, 1);
+    var backtranslation = osg.Matrix.inverse(translation, backtranslation);
+    
+    osg.Matrix.postMult(backtranslation, copy);
+    osg.Matrix.postMult(rotation, copy);
+    osg.Matrix.postMult(translation, copy);
+    
+    arm.setMatrix(copy);
+
+    armNode.removeChildren();
+    armNode.addChild(arm);
+}
+
+
+function createScene(){
+    var root = new osg.Camera();
+    root.setComputeNearFar(false);
+    
+    
+    camera = root;
+    createPickup();
+    return root;
+}
+
+function createPickup(){
+    console.log("create pikup");
+    var root = new osg.MatrixTransform();
+    
+    var models = new osg.Node();
+    console.log("pickupModel : " + pickupModel);
+    models.addChild(pickupModel);
+    
+    var node = new osg.Node();
+    node.addChild(models);
+    
+    root.addChild(node);
+    
+    root.setMatrix(osg.Matrix.makeTranslate(0, 0, -13, []))
+    
+    
+
+    camera.addChild(root);
+    
+    pickup = root;    
+    
+    arm = new osg.MatrixTransform();
+    //    node = new osg.Node();
+    armNode = getNodeFromName(model, "bras");
+    
+    armNode.setNodeMask(~0);
+    armNode.itemToIntersect = true;
+    
+    children = armNode.getChildren();
+    for(var i = 0 ; i < children.length ; i++){
+        arm.addChild(children[i]);
+    }
+//    arm.addChild(node);
+//    node.addChild(armGroup);
+//    arm.addChild(node);
+    
+
+}
+
+function getMk2(){
+    jQuery.ajax({
+        url: "model/Technics_sl1200/TECHNICS_SL1200_triangles.osgjs",  
+        dataType: 'json',   
+        async: false,  
+        success: function(json){  
+            if(json == null){
+                console.log("get null trying to load " + file);
+                return undefined;
+            } else {
+                console.log("success");
+                var parsedScene;
+                parsedScene = osgDB.parseSceneGraph(json);
+                console.log("parsedScene : " + parsedScene);
+                initViewer();
+                return pickupModel;
+            }
+        }  
+    });
+}
+
+function initViewer(){
+    viewer = new osgViewer.Viewer(canvas, {
+        antialias : true, 
+        alpha: false,
+        stats: 1
+    });
+    
+    
+    viewer.init();
+    
+    viewer.setupManipulator();
+    manipulatorOn = true;
+    
+    var scene = createScene();
+    
+    viewer.view.setClearColor([0.3, 0.3, 0.3, 1.0]);
+    viewer.setScene(scene);
+    viewer.getManipulator().computeHomePosition();
+    
+    viewer.run();
+}
+
+function initCanvas(){
+    var size = getWindowSize();
+
+    canvas = document.getElementById("3DView");
+
+    canvas.width = size.w;
+    canvas.height = size.h;
+   
+}
+   
+function getWindowSize() {
+    var myWidth = 0, myHeight = 0;
+    
+    if( typeof( window.innerWidth ) == 'number' ) {
+        //Non-IE
+        myWidth = window.innerWidth;
+        myHeight = window.innerHeight;
+    } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+        //IE 6+ in 'standards compliant mode'
+        myWidth = document.documentElement.clientWidth;
+        myHeight = document.documentElement.clientHeight;
+    } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+        //IE 4 compatible
+        myWidth = document.body.clientWidth;
+        myHeight = document.body.clientHeight;
+    }
+    return {
+        'w': myWidth, 
+        'h': myHeight
+    };
+}
+
