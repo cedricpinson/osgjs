3d7da935dae157a2a1b1f4e3600e757c2935ca6d
Cedric Pinson <cedric.pinson@plopbyte.com>
2012-07-22 18:22:28 +0200

    update noise texture size issue

--- examples/ssao/main.js
+++ examples/ssao/main.js
@@ -557,7 +557,8 @@ function createScene()
                                                 onchange: function(value) {
                                                     // fix to a power of two
                                                     ssao._noiseTextureSize = Math.pow(2,value);
-                                                    ssao.dirty(); 
+                                                    ssao._noiseTextureSize = Math.min(ssao._noiseTextureSize, 512);
+                                                    ssao.dirty();
                                                 },
                                                 html: document.getElementById('parameters')
                                               });
