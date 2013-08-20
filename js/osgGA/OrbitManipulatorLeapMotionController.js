47abd79db4049a9b843a8c060e397d8005122d50
Clément Léger <clement@sketchfab.com>
2013-08-20 12:27:43 +0200

    Two hands zoom for leapmotion.

--- js/osgGA/OrbitManipulatorLeapMotionController.js
+++ js/osgGA/OrbitManipulatorLeapMotionController.js
@@ -7,6 +7,7 @@ osgGA.getOrbitLeapMotionControllerClass = function(module) {
 
     var EnumMode = [ 'getRotateInterpolator',
                      'getPanInterpolator',
+                     'getZoomInterpolator',
                      'getZoomInterpolator'];
     
     var ModeConfig = {
@@ -24,6 +25,11 @@ osgGA.getOrbitLeapMotionControllerClass = function(module) {
             dtx: 0.0,
             dty: -0.5,
             delay: 0.05
+        },
+        'getZoomInterpolator': {
+            dtx: -0.05,
+            dty: 0.0,
+            delay: 0.05
         }
     };
 
@@ -33,9 +39,11 @@ osgGA.getOrbitLeapMotionControllerClass = function(module) {
             this._targetPosition = [0.0,0.0];
             this._previousFrame = undefined;
             this._displacement = [0.0, 0.0];
+            this._top = [0, 1, 0];
             this._motion = [0.0, 0.0];
             this._delay = 0.05;
             this._threshold = 0.08;
+            this._direction_dot_threshold = 0.5;
             this._mode = 0;
         },
 
@@ -90,10 +98,23 @@ osgGA.getOrbitLeapMotionControllerClass = function(module) {
                     // by default onw hand moving means rotation
                     mode = 0;
                 }
-            }
+            } else if (frame.hands.length === 2) {
+                var d0 = Math.abs(osg.Vec3.dot(frame.hands[0].palmNormal, this._top)),
+                    d1 = Math.abs(osg.Vec3.dot(frame.hands[1].palmNormal, this._top));
 
+                // two hands : zoom
+                if (d0 < this._direction_dot_threshold && d1 < this._direction_dot_threshold) {
+                    mode = 3;
+                } else {
+                    // if hands flat do nothing
+                    mode = -1;
+                    this.hands_distance_old = undefined;
+                }
+            }
             var zoom  = this._manipulator.getZoomInterpolator();
 
+            if(mode === -1)
+                return;
             // change mode reset counter and skip this frame
             var enumMode = EnumMode[mode];
             if (mode !== this._mode) {
@@ -125,8 +146,22 @@ osgGA.getOrbitLeapMotionControllerClass = function(module) {
             this._manipulator[enumMode]().setDelay(delay);
 
             if (enumMode === 'getZoomInterpolator') {
-                osg.log(dist);
-                zoom.setTarget(dist);
+                if (mode === 2) {
+                    osg.log(dist);
+                    zoom.setTarget(dist);
+                } else if (mode === 3) { // two hands zoom
+                    // distance between two hands
+                    var hands_distance = osg.Vec3.distance(frame.hands[0].palmPosition, frame.hands[1].palmPosition);
+
+                    if (this.hands_distance_old !== undefined) {
+                        // compare distance with lastframe and zoom if they get nearer, unzoom if they separate
+                        var vel = dtx * (hands_distance - this.hands_distance_old);
+
+                        dist = zoom._target;
+                        dist[0] += vel;
+                    }
+                    this.hands_distance_old = hands_distance;
+                }
             } else {
                 this._manipulator[enumMode]().addTarget(this._motion[0], this._motion[1]);
             }
