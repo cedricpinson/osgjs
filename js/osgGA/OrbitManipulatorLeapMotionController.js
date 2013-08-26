b2603f4e10deb6a8f9c6f75b52dbe12631d6d3ed
Clément Léger <clement@sketchfab.com>
2013-08-26 15:30:05 +0200

    Leap motion orbit refactor.
    String as mode designation.

--- js/osgGA/OrbitManipulatorLeapMotionController.js
+++ js/osgGA/OrbitManipulatorLeapMotionController.js
@@ -5,31 +5,30 @@ osgGA.getOrbitLeapMotionControllerClass = function(module) {
         this.init();
     };
 
-    var EnumMode = [ 'getRotateInterpolator',
-                     'getPanInterpolator',
-                     'getZoomInterpolator',
-                     'getZoomInterpolator'];
-    
     var ModeConfig = {
-        'getRotateInterpolator': {
+        'rotate': {
             dtx: -1.2*1.2,
             dty: -0.9*1.2,
-            delay: 0.05
+            delay: 0.05,
+            method: 'getRotateInterpolator'
         },
-        'getPanInterpolator': {
+        'pan': {
             dtx: -1.2*1.2,
             dty: -0.9*1.2,
-            delay: 0.05
+            delay: 0.05,
+            method: 'getPanInterpolator'
         },
-        'getZoomInterpolator': {
+        'zoom': {
             dtx: 0.0,
             dty: -0.5,
-            delay: 0.05
+            delay: 0.05,
+            method: 'getZoomInterpolator'
         },
-        'getZoomInterpolator': {
+        'zoom-twohands': {
             dtx: -0.05,
             dty: 0.0,
-            delay: 0.05
+            delay: 0.05,
+            method: 'getZoomInterpolator'
         }
     };
 
@@ -44,7 +43,7 @@ osgGA.getOrbitLeapMotionControllerClass = function(module) {
             this._delay = 0.05;
             this._threshold = 0.08;
             this._direction_dot_threshold = 0.5;
-            this._mode = 0;
+            this._mode = 'rotate';
         },
 
         update: function(frame) {
@@ -73,7 +72,6 @@ osgGA.getOrbitLeapMotionControllerClass = function(module) {
 
             var mode = this._mode;
             var dist = 0;
-            var scaleFactor = 1.0;
 
             // scale is when there two hands with but with two hand with more than 1 fingers
             if (frame.gestures.length > 0) {
@@ -88,15 +86,15 @@ osgGA.getOrbitLeapMotionControllerClass = function(module) {
 
             if (frame.hands.length === 1) {
                 if ( frame.hands[0].fingers.length >= 3) {
-                    mode = 2;
+                    mode = 'zoom';
                     dist = frame.hands[0].palmPosition[1]/10.0;
                     dist = Math.max(dist-4,0.01);
 
                 } else if ( frame.hands[0].fingers.length > 1) {
-                    mode = 1;
+                    mode = 'pan';
                 } else {
                     // by default onw hand moving means rotation
-                    mode = 0;
+                    mode = 'rotate';
                 }
             } else if (frame.hands.length === 2) {
                 var d0 = Math.abs(osg.Vec3.dot(frame.hands[0].palmNormal, this._top)),
@@ -104,27 +102,27 @@ osgGA.getOrbitLeapMotionControllerClass = function(module) {
 
                 // two hands : zoom
                 if (d0 < this._direction_dot_threshold && d1 < this._direction_dot_threshold) {
-                    mode = 3;
+                    mode = 'zoom-twohands';
                 } else {
                     // if hands flat do nothing
-                    mode = -1;
+                    mode = undefined;
                     this.hands_distance_old = undefined;
                 }
             }
             var zoom  = this._manipulator.getZoomInterpolator();
 
-            if(mode === -1)
+            if (mode === undefined) {
                 return;
+            }
             // change mode reset counter and skip this frame
-            var enumMode = EnumMode[mode];
             if (mode !== this._mode) {
-                osg.info("Switch to mode " + enumMode.toString());
+                osg.info("Switch to mode " + mode);
 
                 this._motion[0] = 0;
                 this._motion[1] = 0;
                 this._mode = mode;
 
-                if (enumMode === 'getZoomInterpolator') {
+                if (mode === 'zoom' || mode === 'zoom-twohands') {
                     if (zoom.isReset()) {
                         zoom._start = 1.0;
                         zoom.set(0.0);
@@ -134,36 +132,35 @@ osgGA.getOrbitLeapMotionControllerClass = function(module) {
             }
 
             var dtx,dty;
-            dtx = ModeConfig[enumMode].dtx;
-            dty = ModeConfig[enumMode].dty;
+            dtx = ModeConfig[mode].dtx;
+            dty = ModeConfig[mode].dty;
 
             this._motion[0] += deltaFrame[0]*dtx;
             this._motion[1] += deltaFrame[1]*dty;
 
-            var delay = ModeConfig[enumMode].delay;
+            var delay = ModeConfig[mode].delay;
             
             // we use the mode enum to get the good method
-            this._manipulator[enumMode]().setDelay(delay);
+            var method = ModeConfig[mode].method;
+            this._manipulator[method]().setDelay(delay);
 
-            if (enumMode === 'getZoomInterpolator') {
-                if (mode === 2) {
-                    osg.log(dist);
-                    zoom.setTarget(dist);
-                } else if (mode === 3) { // two hands zoom
-                    // distance between two hands
-                    var hands_distance = osg.Vec3.distance(frame.hands[0].palmPosition, frame.hands[1].palmPosition);
+            if (mode === 'zoom') {
+                osg.log(dist);
+                zoom.setTarget(dist);
+            } else if (mode === 'zoom-twohands') { // two hands zoom
+                // distance between two hands
+                var hands_distance = osg.Vec3.distance(frame.hands[0].palmPosition, frame.hands[1].palmPosition);
 
-                    if (this.hands_distance_old !== undefined) {
-                        // compare distance with lastframe and zoom if they get nearer, unzoom if they separate
-                        var vel = dtx * (hands_distance - this.hands_distance_old);
+                if (this.hands_distance_old !== undefined) {
+                    // compare distance with lastframe and zoom if they get nearer, unzoom if they separate
+                    var vel = dtx * (hands_distance - this.hands_distance_old);
 
-                        dist = zoom._target;
-                        dist[0] += vel;
-                    }
-                    this.hands_distance_old = hands_distance;
+                    dist = zoom._target;
+                    dist[0] += vel;
                 }
+                this.hands_distance_old = hands_distance;
             } else {
-                this._manipulator[enumMode]().addTarget(this._motion[0], this._motion[1]);
+                this._manipulator[method]().addTarget(this._motion[0], this._motion[1]);
             }
             
             this._motion[1] = this._motion[0] = 0;
