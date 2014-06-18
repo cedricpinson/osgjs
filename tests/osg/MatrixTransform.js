5fbc049a7b9daf9e19256da7478fc4c7e03f3aef
Stéphane GINIER <stephane@sketchfab.com>
2014-06-18 18:10:38 +0200

    Fixes bounding sphere computation

--- tests/osg/MatrixTransform.js
+++ tests/osg/MatrixTransform.js
@@ -5,6 +5,8 @@ define( [
     'osgDB/ReaderParser'
 ], function ( mockup, MatrixTransform, Matrix, ReaderParser ) {
 
+    'use strict';
+
     return function () {
 
         module( 'osg' );
@@ -18,6 +20,17 @@ define( [
             var bs = n.getBound();
             mockup.near( bs.center(), [ 100, 0, 0 ] );
             mockup.near( bs.radius(), 2.414213562373095 );
+        } );
+
+        test( 'Transform', function () {
+
+            var n = new MatrixTransform();
+            var scene = ReaderParser.parseSceneGraph( mockup.getBoxScene() );
+            Matrix.makeScale( 2, 3, 4, n.getMatrix() );
+            n.addChild( scene );
+            var bs = n.getBound();
+            mockup.near( bs.center(), [ 0, 0, 0 ] );
+            mockup.near( bs.radius(), 9.65685424949238 );
         } );
     };
 } );
