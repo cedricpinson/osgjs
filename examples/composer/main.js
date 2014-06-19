78aa0443f57b1f1b2231bf14f6ad1ea015163b26
Thibault D'Archivio <thibault.darchivio@free.fr>
2014-06-19 18:07:18 +0200

    Added sharpen filter to examples

--- examples/composer/main.js
+++ examples/composer/main.js
@@ -326,6 +326,112 @@ function createPostSceneBloom( sceneTexture, textureSize, bloomTextureFactor ) {
     return scene;
 }
 
+var setFactor;
+var enableDiagonal;
+
+function createPostSceneSharpen(textureSize) {
+
+    var scene = new osg.MatrixTransform();
+
+    var input_texture = osg.Texture.createFromURL('Medusa.png');
+
+    // create a texture to render the effect to
+    var final_texture = new osg.Texture();
+    final_texture.setTextureSize( textureSize[0], textureSize[1] );
+    final_texture.setMinFilter( 'LINEAR' );
+    final_texture.setMagFilter( 'LINEAR' );
+
+    var kernel = osg.Uniform.createMatrix3(laplace(1), 'kernel');
+    var use_diagonal = false;
+    var factor = 0;
+
+    setFactor = function(x) {
+        factor = x;
+        updateKernel();
+    };
+    
+    enableDiagonal = function(bool) {
+        use_diagonal = bool;
+        updateKernel();
+    };
+
+    function updateKernel() {
+
+        if (use_diagonal)
+            kernel.set(laplace(factor));
+        else
+            kernel.set(laplace_diagonal(factor));
+    }   
+
+    // Discrete Laplace convolution kernels from 
+    // http://en.wikipedia.org/wiki/Laplace_filter#Implementation_in_Image_Processing
+    function laplace(x) {return [0, -x, 0, -x, x*4, -x, 0, -x, 0]; }
+
+    function laplace_diagonal(x) {return [-0.5*x, -x, -0.5*x, -x, x*6, -x, -0.5*x, -x, -0.5*x]; }
+
+    // For each texel, we sum the difference with its neighboors
+    // and add this computed difference to the original texel
+    // which produce a 'sharpened' look
+    var sharpenFilter = new osgUtil.Composer.Filter.Custom(
+        [
+        '#ifdef GL_ES',
+        'precision highp float;',
+        '#endif',
+
+        'varying vec2 FragTexCoord0;',
+        'uniform sampler2D input_texture;',
+        'uniform vec2 RenderSize;',
+        'uniform mat3 kernel;',
+
+        'float stepX = 1.0/RenderSize.x;',
+        'float stepY = stepX;',
+
+        'void main() {',
+
+            'vec4 color = texture2D(input_texture, FragTexCoord0);',
+
+            'vec2 uv = FragTexCoord0;',
+            'vec2 offset = vec2(0.0);',
+
+            'for (int i=0; i < 3; i++)',
+            '{',
+                'offset.x = (1.0 - float(i)) * stepX;',
+                'for (int j = 0; j < 3; j++)',
+                '{',
+                    'offset.y = (1.0 - float(j)) * stepY;',
+
+                    'color += kernel[i][j] * texture2D(input_texture, FragTexCoord0 + offset);',
+                '}',
+            '}',
+
+        'gl_FragColor = color;',
+        '}',
+        ].join('\n'),
+        {
+            'kernel': kernel,
+            'input_texture': input_texture
+        }
+    );
+    // create a textured quad on which is applied the final texture
+    var quadSize = [ 16 / 9, 1 ];
+    var quad = osg.createTexturedQuadGeometry(  -quadSize[ 0 ] / 2.0, 0, -quadSize[ 1 ] / 2.0,
+                                                quadSize[ 0 ]       , 0, 0,
+                                                0                   , 0, quadSize[ 1 ] );
+
+    quad.getOrCreateStateSet().setAttributeAndMode( getTextureShader() );
+    quad.getOrCreateStateSet().setTextureAttributeAndMode( 0, final_texture );
+
+    var composer = new osgUtil.Composer();
+
+    composer.addPass(sharpenFilter, final_texture);
+
+    composer.build();
+
+    scene.addChild(quad);
+    scene.addChild(composer);
+
+    return scene;
+}
 
 function commonScene( rttSize ) {
 
@@ -413,6 +519,12 @@ function createScene() {
         root.addChild(scene);
     }
 
+    if (true) {
+        scene = createPostSceneSharpen(rttSize);
+        scene.setMatrix(osg.Matrix.makeTranslate(2.0, 0.0, -1.25, []));
+        root.addChild(scene);
+    }
+
     return root;
 }
 
@@ -459,6 +571,7 @@ function buildVignettegGui(mainGui) {
         lensRadius.dirty();
     });
 }
+
 function buildBloomGui(mainGui) {
 
     var folder = mainGui.addFolder('Bloom');
@@ -480,6 +593,28 @@ function buildBloomGui(mainGui) {
     });
 }
 
+function buildSharpenGui(mainGui) {
+
+    var folder = mainGui.addFolder('Sharpen');
+
+    var kernel = {
+        'kernel': 0,
+        'sample diagonal': false
+    };
+
+    var kernel_controller = folder.add(kernel, 'kernel', 0, 5.0);
+    var diagonal_controller = folder.add(kernel, 'sample diagonal');
+
+    kernel_controller.onChange(function ( value ) {
+        setFactor(value);
+    });
+    diagonal_controller.onChange(function ( bool ) {
+        enableDiagonal(bool);
+    });
+
+
+}
+
 var main = function () {
 
     // osg.ReportWebGLError = true;
@@ -493,7 +628,8 @@ var main = function () {
     buildStitchingGui(gui);
     buildVignettegGui(gui);
     buildBloomGui(gui);
-
+    buildSharpenGui(gui);
+    
     document.getElementById( 'gui' ).appendChild( gui.domElement );
 
     var rotate = new osg.MatrixTransform();
