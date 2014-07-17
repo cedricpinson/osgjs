076d347e417f86a7eb2519e8bafb5e1718e6f7ba
Cedric Pinson <cedric@plopbyte.com>
2014-07-17 23:24:50 +0200

    Updates test of examples

--- examples/pokerscene/main.js
+++ examples/pokerscene/main.js
@@ -1,65 +1,44 @@
-/** -*- compile-command: "jslint-cli main.js" -*-
- *
- * Copyright (C) 2011 Cedric Pinson
- *
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- * Authors:
- *  Cedric Pinson <cedric.pinson@plopbyte.com>
- *
- */
+(function() {
+    'use strict';
 
-'use strict';
+    var OSG = window.OSG;
+    OSG.globalify();
+    var osg = window.osg;
+    var osgDB = window.osgDB;
+    var osgViewer = window.osgViewer;
+    var getPokerScene = window.getPokerScene;
 
-var OSG = window.OSG;
-OSG.globalify();
-var osg = window.osg;
-var osgDB = window.osgDB;
-var osgViewer = window.osgViewer;
+    function createScene() {
+        var root = new osg.Node();
+        // override texture constructor to set the wrap mode repeat for all texture
+        var previousTextureDefault = osg.Texture.prototype.setDefaultParameters;
+        osg.Texture.prototype.setDefaultParameters = function () {
+            previousTextureDefault.call( this );
+            this.setWrapS( 'REPEAT' );
+            this.setWrapT( 'REPEAT' );
+            this.setMagFilter( 'LINEAR' );
+            this.setMinFilter( 'LINEAR_MIPMAP_LINEAR' );
+        };
 
-function createScene() {
-    var root = new osg.Node();
-    // override texture constructor to set the wrap mode repeat for all texture
-    var previousTextureDefault = osg.Texture.prototype.setDefaultParameters;
-    osg.Texture.prototype.setDefaultParameters = function () {
-        previousTextureDefault.call( this );
-        this.setWrapS( 'REPEAT' );
-        this.setWrapT( 'REPEAT' );
-        this.setMagFilter( 'LINEAR' );
-        this.setMinFilter( 'LINEAR_MIPMAP_LINEAR' );
-    };
+        Q.when( osgDB.parseSceneGraph( getPokerScene() ) ).then( function ( child ) {
+            root.addChild( child );
+        } );
+        return root;
+    }
 
-    Q.when( osgDB.parseSceneGraph( getPokerScene() ) ).then( function ( child ) {
-        root.addChild( child );
-    } );
-    return root;
-}
+    window.addEventListener(
+        'load',
 
-window.addEventListener(
-    'load',
+        function () {
 
-    function () {
+            var canvas = document.getElementById( 'View' );
 
-        var canvas = document.getElementById( 'View' );
+            var viewer;
+            viewer = new osgViewer.Viewer( canvas );
+            viewer.init();
+            viewer.setSceneData( createScene() );
+            viewer.setupManipulator();
+            viewer.run();
 
-        var viewer;
-        viewer = new osgViewer.Viewer( canvas );
-        viewer.init();
-        viewer.setSceneData( createScene() );
-        viewer.setupManipulator();
-        viewer.run();
-
-    }, true );
+        }, true );
+})();
