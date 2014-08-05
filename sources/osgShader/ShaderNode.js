9859786aef84dab55e8d8c6109c7a02ae961f39c
Cedric Pinson <cedric@plopbyte.com>
2014-08-05 22:57:15 +0200

    Beautify the code

--- sources/osgShader/ShaderNode.js
+++ sources/osgShader/ShaderNode.js
@@ -1,30 +1,35 @@
-define ( [
+define( [
+
     'osgShader/shaderNode/data',
     'osgShader/shaderNode/textures',
     'osgShader/shaderNode/functions',
     'osgShader/shaderNode/lights',
     'osgShader/shaderNode/operations'
 
-], function ( data, textures, functions, lights, operations ) {
+], function ( /*data, textures, functions, lights, operations */) {
     'use strict';
 
     var lib = {};
 
     // use sublib except _ of course
-    var subnamespace = Array.prototype.slice.call(arguments, 0);
+    var subnamespace = Array.prototype.slice.call( arguments, 0 );
 
     // add all sub component to root level of the lib
-    subnamespace.forEach( function( component, index ) {
-        var shaderNodeEntries = Object.keys(component);
-        shaderNodeEntries.forEach( function( key ) {
-            var element = component[key];
-            if ( this[key] !== undefined ) { // if exist throw exception
+    subnamespace.forEach( function ( component /*, index */ ) {
+
+        Object.keys( component ).forEach( function ( key ) {
+
+            var element = component[ key ];
+
+            if ( this[ key ] !== undefined ) { // if exist throw exception
                 throw 'duplicate entry in ShaderNode library';
             }
+
             this[ key ] = element;
-        }, this);
+
+        }, this );
 
     }, lib );
 
     return lib;
-});
+} );
