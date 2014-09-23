a96be6d445b78630e8e9bf486d5da9c95352c718
Cedric Pinson <cedric@plopbyte.com>
2014-09-23 19:44:35 +0200

    Clean diff

--- examples/camera/main.js
+++ examples/camera/main.js
@@ -1,4 +1,4 @@
-( function () {
+(function() {
     'use strict';
 
     window.OSG.globalify();
@@ -21,8 +21,8 @@
         // we will render a textured quad on the rtt target with a fixed texture without
         // motion
         var textureQuad = osg.createTexturedQuadGeometry( 0, 0, 0,
-            rttSize[ 0 ], 0, 0,
-            0, rttSize[ 1 ], 0 );
+                                                  rttSize[ 0 ], 0, 0,
+                                                  0, rttSize[ 1 ], 0 );
         textureQuad.getOrCreateStateSet().setTextureAttributeAndMode( 0, osg.Texture.createFromURL( 'textures/sol_trauma_periph.png' ) );
         rttCamera.addChild( textureQuad );
 
@@ -39,8 +39,8 @@
         // for this we will create a textured quad that will use the rtt
         // target texture
         var texturedQuadUsingTargetTexture = osg.createTexturedQuadGeometry( -25, -25, 0,
-            50, 0, 0,
-            0, 50, 0 );
+                                                                     50, 0, 0,
+                                                                     0, 50, 0 );
         texturedQuadUsingTargetTexture.getOrCreateStateSet().setTextureAttributeAndMode( 0, rttTargetTexture );
 
         var root = new osg.Node();
@@ -111,4 +111,4 @@
     //   appendScript ('../../builds/active/OSG.js');
     //   window.addEventListener('load', main, true);
     // }
-} )();
+})();
