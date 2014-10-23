ce248a22e52da1f8a129e79d49a2d4d30bd52c33
Rémy Alves <remy@sketchfab.com>
2014-10-23 15:20:59 +0200

    [#79107626] Refactors the code

--- examples/scene-debug/debugVisitor.js
+++ examples/scene-debug/debugVisitor.js
@@ -1,81 +1,129 @@
 var osg = window.OSG.osg;
-var nodeListSize = 0;
-var fullNodeList = [];
-var nodeList = [];
-var linkListSize = 0;
-var linkList = [];
 
-var DebugVisitor = function ( /* name */) {
+var DebugVisitor = function () {
     osg.NodeVisitor.call( this, osg.NodeVisitor.TRAVERSE_ALL_CHILDREN );
+
+    this._nodeListSize = 0;
+    this._fullNodeList = [];
+    this._nodeList = [];
+    this._linkListSize = 0;
+    this._linkList = [];
 };
 
 DebugVisitor.prototype = osg.objectInherit( osg.NodeVisitor.prototype, {
+
     apply: function ( node ) {
 
-        if ( fullNodeList[ node.getInstanceID() ] !== node ) {
-            
+        if ( this._fullNodeList[ node.getInstanceID() ] !== node ) {
+
             var nodeMatrix = '';
             if ( node.matrix ) {
-                var maxSize = 0;
-                var firstRowMaxSize = 0;
-                var i;
-                for ( i = 0; i < 16; i++ ) {
-                    if ( ( node.matrix[ i ] + '' ).length > maxSize ) {
-                        maxSize = ( node.matrix[ i ] + '' ).length;
-                    }
-                    if ( i % 4 === 0 && ( node.matrix[ i ] + '' ).length > firstRowMaxSize ) {
-                        firstRowMaxSize = ( node.matrix[ i ] + '' ).length;
-                    }
-                }
-                maxSize++;
-                for ( i = 0; i < 16; i++ ) {
-                    if ( i % 4 === 0 ) {
-                        for ( j = ( node.matrix[ i ] + '' ).length; j < firstRowMaxSize; j++ ) {
-                            nodeMatrix += ' ';
-                        }
-                    } else {
-                        for ( j = ( node.matrix[ i ] + '' ).length; j < maxSize; j++ ) {
-                            nodeMatrix += ' ';
-                        }
-                    }
-                    nodeMatrix += node.matrix[ i ];
-                    if ( ( i + 1 ) % 4 === 0 ) {
-                        nodeMatrix += '<br />';
-                    }
-                }
+                nodeMatrix = this.createMatrixGrid( node, nodeMatrix );
             }
             var stateset = null;
             if ( node.stateset ) {
-                stateset = {
-                    name: 'StateSet - ' + node.getInstanceID(),
-                    statesetID: node.stateset.getInstanceID(),
-                    parentID: node.getInstanceID(),
-                    stateset: node.stateset,
-                    numTexture: node.stateset.getNumTextureAttributeLists()
-                };
+                stateset = this.createStateset( node, stateset );
             }
 
-            fullNodeList[ node.getInstanceID() ] = node;
+            this._fullNodeList[ node.getInstanceID() ] = node;
 
-            nodeList[ nodeListSize ] = {
+            this._nodeList[ this._nodeListSize ] = {
                 name: node.getName(),
                 className: node.className(),
                 instanceID: node.getInstanceID(),
                 stateset: stateset,
                 matrix: nodeMatrix
             };
-            nodeListSize++;
+            this._nodeListSize++;
 
         }
 
-        for ( var childID in node.children ) {
-            linkList[ linkListSize ] = {
-                parentNode: node.getInstanceID(),
-                childrenNode: node.children[ childID ].getInstanceID()
-            };
-            linkListSize++;
+        var keys = Object.keys( node.children );
+        if ( keys.length > 0 ) {
+            for ( var i = 0, l = keys.length; i < l; i++ ) {
+                var key = keys[ i ];
+                this._linkList[ this._linkListSize ] = {
+                    parentNode: node.getInstanceID(),
+                    childrenNode: node.children[ key ].getInstanceID()
+                };
+                this._linkListSize++;
+            }
         }
 
         this.traverse( node );
+    },
+
+    reset: function () {
+        this._nodeListSize = 0;
+        this._fullNodeList = [];
+        this._nodeList = [];
+        this._linkListSize = 0;
+        this._linkList = [];
+    },
+
+    // Create a good sized array to display the matrix
+    createMatrixGrid: function ( node, nodeMatrix ) {
+        var maxSize = 0;
+        var firstRowMaxSize = 0;
+        var i;
+        for ( i = 0; i < 16; i++ ) {
+            if ( ( node.matrix[ i ] + '' ).length > maxSize ) {
+                maxSize = ( node.matrix[ i ] + '' ).length;
+            }
+            if ( i % 4 === 0 && ( node.matrix[ i ] + '' ).length > firstRowMaxSize ) {
+                firstRowMaxSize = ( node.matrix[ i ] + '' ).length;
+            }
+        }
+        maxSize++;
+        for ( i = 0; i < 16; i++ ) {
+            var j;
+            if ( i % 4 === 0 ) {
+                for ( j = ( node.matrix[ i ] + '' ).length; j < firstRowMaxSize; j++ ) {
+                    nodeMatrix += ' ';
+                }
+            } else {
+                for ( j = ( node.matrix[ i ] + '' ).length; j < maxSize; j++ ) {
+                    nodeMatrix += ' ';
+                }
+            }
+            nodeMatrix += node.matrix[ i ];
+            if ( ( i + 1 ) % 4 === 0 ) {
+                nodeMatrix += '<br />';
+            }
+        }
+        return nodeMatrix;
+    },
+
+    // Get the stateset and create the stateset display structure
+    createStateset: function ( node, stateset ) {
+        stateset = {
+            name: 'StateSet - ' + node.getInstanceID(),
+            statesetID: node.stateset.getInstanceID(),
+            parentID: node.getInstanceID(),
+            stateset: node.stateset,
+            numTexture: node.stateset.getNumTextureAttributeLists()
+        };
+        return stateset;
+    },
+
+    getNodeListSize: function () {
+        return this._nodeListSize;
+    },
+
+    getFullNodeList: function () {
+        return this._fullNodeList;
+    },
+
+    getNodeList: function () {
+        return this._nodeList;
+    },
+
+    getLinkListSize: function () {
+        return this._linkListSize;
+    },
+
+    getLinkList: function () {
+        return this._linkList;
     }
+
 } );
--- examples/scene-debug/main.js
+++ examples/scene-debug/main.js
@@ -1,240 +1,261 @@
-var OSG = window.OSG;
-OSG.globalify();
-var osg = window.osg;
-var osgViewer = window.osgViewer;
+( function () {
+    'use strict';
 
-// Simple tooltips
-function SimpleTooltips( options ) {
+    var OSG = window.OSG;
+    OSG.globalify();
+    var osg = OSG.osg;
+    var osgViewer = OSG.osgViewer;
+    var $ = window.$;
 
-    this.options = options;
+    var Example = function () {};
 
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = [
-        ".simple-tooltip {",
-            "display: none;",
-            "position: absolute;",
-            "margin-left: 10px;",
-            "border-radius: 4px;",
-            "padding: 10px;",
-            "background: rgba(0,0,0,.9);",
-            "color: #ffffff;",
-        "}",
-        ".simple-tooltip:before {",
-            "content: ' ';",
-            "position: absolute;",
-            "left: -10px;",
-            "top: 8px;",
-            "border: 10px solid transparent;",
-            "border-width: 10px 10px 10px 0;",
-            "border-right-color: rgba(0,0,0,.9);",
-        "}"
-    ].join('\n');
-    document.getElementsByTagName("head")[0].appendChild(css);
+    Example.prototype = {
 
-    this.el = document.createElement('div');
-    this.el.className = 'simple-tooltip';
-    document.body.appendChild(this.el);
+        // Simple tooltips implementation
+        SimpleTooltips: function ( options ) {
+            this.options = options;
 
-    function showTooltip(e) {
-        var target = e.currentTarget;
-        this.el.innerHTML = target.getAttribute('title');
-        this.el.style.display = 'block';
-        this.el.style.left = ($(target).position().left + $(target).get(0).getBoundingClientRect().width) + 'px';
-        this.el.style.top = $(target).position().top + 'px';
-    }
+            var css = document.createElement( "style" );
+            css.type = "text/css";
+            css.innerHTML = [
+                ".simple-tooltip {",
+                "display: none;",
+                "position: absolute;",
+                "margin-left: 10px;",
+                "border-radius: 4px;",
+                "padding: 10px;",
+                "background: rgba(0,0,0,.9);",
+                "color: #ffffff;",
+                "}",
+                ".simple-tooltip:before {",
+                "content: ' ';",
+                "position: absolute;",
+                "left: -10px;",
+                "top: 8px;",
+                "border: 10px solid transparent;",
+                "border-width: 10px 10px 10px 0;",
+                "border-right-color: rgba(0,0,0,.9);",
+                "}"
+            ].join( '\n' );
+            document.getElementsByTagName( "head" )[ 0 ].appendChild( css );
 
-    function hideTooltip(e) {
-        this.el.style.display = 'none';
-    }
+            this.el = document.createElement( 'div' );
+            this.el.className = 'simple-tooltip';
+            document.body.appendChild( this.el );
 
-    var nodes = document.querySelectorAll( this.options.selector );
-    for (var i=0; i<nodes.length; i++) {
-        nodes[i].addEventListener('mouseover', showTooltip.bind(this), false);
-        nodes[i].addEventListener('mouseout', hideTooltip.bind(this), false);
-    }
-}
+            function showTooltip( e ) {
+                var target = e.currentTarget;
+                this.el.innerHTML = target.getAttribute( 'title' );
+                this.el.style.display = 'block';
+                this.el.style.left = ( $( target ).position().left + $( target ).get( 0 ).getBoundingClientRect().width ) + 'px';
+                this.el.style.top = $( target ).position().top + 'px';
+            }
 
-var main = function () {
-    var canvas = document.getElementById( 'View' );
+            function hideTooltip( e ) {
+                this.el.style.display = 'none';
+            }
 
-    var viewer;
-    try {
-        viewer = new osgViewer.Viewer( canvas, {
-            antialias: true,
-            alpha: true
-        } );
-        viewer.init();
-        var rotate = new osg.MatrixTransform();
-        rotate.addChild( createScene() );
-        viewer.getCamera().setClearColor( [ 0.0, 0.0, 0.0, 0.0 ] );
-        viewer.setSceneData( rotate );
-        viewer.setupManipulator();
-        viewer.getManipulator().computeHomePosition();
+            var nodes = document.querySelectorAll( this.options.selector );
+            for ( var i = 0; i < nodes.length; i++ ) {
+                nodes[ i ].addEventListener( 'mouseover', showTooltip.bind( this ), false );
+                nodes[ i ].addEventListener( 'mouseout', hideTooltip.bind( this ), false );
+            }
+        },
 
-        viewer.run();
+        // Create and display a dagre d3 graph
+        createGraph: function ( visitor ) {
 
-    } catch ( er ) {
-        osg.log( 'exception in osgViewer ' + er );
-    }
+            var g = new dagreD3.Digraph();
 
-    $( '.button' ).click( function () {
-        if ( $( 'svg' ).css( 'zIndex' ) === '-1' ) {
-            $( '.button' ).text( 'Access to the scene' );
-            $( 'svg' ).css( 'zIndex', '1' );
-        } else {
-            $( '.button' ).text( 'Access to the graph' );
-            $( 'svg' ).css( 'zIndex', '-1' );
-        }
-    } );
+            g = this.generateNodeAndLink( visitor, g );
 
-};
+            // Create the renderer
+            var renderer = new dagreD3.Renderer();
 
-function createGraph() {
+            // Set up an SVG group so that we can translate the final graph.
+            var svg = d3.select( 'svg' ),
+                svgGroup = svg.append( 'g' );
 
-    var g = new dagreD3.Digraph();
+            // Set initial zoom to 75%
+            var initialScale = 0.75;
+            var oldZoom = renderer.zoom();
+            renderer.zoom( function ( graph, svg ) {
+                var zoom = oldZoom( graph, svg );
 
-    for ( i = 0; i < nodeListSize; i++ ) {
-        g.addNode( nodeList[ i ].instanceID, {
-            label: nodeList[ i ].className + ( nodeList[ i ].name ? '\n' + nodeList[ i ].name : '' ),
-            description: ( nodeList[ i ].stateset != null ? 'StateSetID : ' + nodeList[ i ].stateset.statesetID : '' ) + ( nodeList[ i ].stateset != null && nodeList[ i ].matrix !== '' ? '<br /><br />' : '' ) + nodeList[ i ].matrix
-        } );
-
-        if ( nodeList[ i ].stateset != null ) {
-            g.addNode( nodeList[ i ].stateset.name, {
-                label: 'StateSet',
-                description: 'numTexture : ' + nodeList[ i ].stateset.numTexture,
-                style: 'fill: #0099FF;stroke-width: 0px;'
+                zoom.scale( initialScale ).event( svg );
+                return zoom;
             } );
 
-            g.addEdge( null, nodeList[ i ].instanceID, nodeList[ i ].stateset.name, {
-                style: 'stroke: #0099FF;'
+            // Simple function to style the tooltip for the given node.
+            var styleTooltip = function ( name, description ) {
+                return '<p class="name">' + name + '</p><pre class="description">' + description + '</pre>';
+            };
+
+            // Override drawNodes to set up the hover.
+            var oldDrawNodes = renderer.drawNodes();
+            renderer.drawNodes( function ( g, svg ) {
+                var svgNodes = oldDrawNodes( g, svg );
+
+                // Set the title on each of the nodes and use tipsy to display the tooltip on hover
+                svgNodes.attr( 'title', function ( d ) {
+                    return styleTooltip( d, g.node( d ).description );
+                } );
+
+                return svgNodes;
+            } );
+
+            // Run the renderer. This is what draws the final graph.
+            var layout = renderer.run( g, svgGroup );
+
+            new this.SimpleTooltips( {
+                selector: '.node'
+            } );
+
+            // Do a console log of the node (or stateset) and save it in Window.*
+            $( '.node' ).click( function () {
+                var identifier = $( this ).attr( 'title' ).split( '<' )[ 1 ].split( '>' )[ 1 ];
+                if ( identifier.search( 'StateSet' ) === -1 ) {
+                    window.activeNode = visitor.getFullNodeList()[ identifier ];
+                    console.log( 'window.activeNode is set.' );
+                    console.log( visitor.getFullNodeList()[ identifier ] );
+                } else {
+                    var stateset = visitor.getFullNodeList()[ identifier.split( ' ' )[ 2 ] ].stateset;
+                    window.activeStateset = stateset;
+                    console.log( 'window.activeStateset is set.' );
+                    console.log( stateset );
+
+                }
+
+            } );
+        },
+
+        // Subfunction of createGraph, will iterate to create all the node and link in dagre
+        generateNodeAndLink: function ( visitor, g ) {
+            for ( var i = 0; i < visitor.getNodeListSize(); i++ ) {
+                var element = visitor.getNodeList()[ i ];
+
+                g.addNode( element.instanceID, {
+                    label: element.className + ( element.name ? '\n' + element.name : '' ),
+                    description: ( element.stateset != null ? 'StateSetID : ' + element.stateset.statesetID : '' ) + ( element.stateset != null && element.matrix !== '' ? '<br /><br />' : '' ) + element.matrix
+                } );
+
+                if ( element.stateset != null ) {
+
+                    g.addNode( element.stateset.name, {
+                        label: 'StateSet',
+                        description: 'numTexture : ' + element.stateset.numTexture,
+                        style: 'fill: #0099FF;stroke-width: 0px;'
+                    } );
+
+                    g.addEdge( null, element.instanceID, element.stateset.name, {
+                        style: 'stroke: #0099FF;'
+                    } );
+                }
+            }
+
+            for ( i = 0; i < visitor.getLinkListSize(); i++ ) {
+                g.addEdge( null, visitor.getLinkList()[ i ].parentNode, visitor.getLinkList()[ i ].childrenNode );
+            }
+
+            return g;
+        },
+
+        // This function will create a basic scene with some cubes
+        createScene: function () {
+            var root = new osg.Node();
+
+            var group1 = new osg.MatrixTransform();
+            var group21 = new osg.MatrixTransform();
+            var group22 = new osg.MatrixTransform();
+            var group3 = new osg.MatrixTransform();
+            var group31 = new osg.MatrixTransform();
+            var group32 = new osg.MatrixTransform();
+            var group4 = new osg.MatrixTransform();
+
+            group1.setMatrix( osg.Matrix.makeTranslate( +5, 10, -5 ) );
+            group21.setMatrix( osg.Matrix.makeTranslate( 0, 10, 0 ) );
+            group22.setMatrix( osg.Matrix.makeTranslate( 10, 0, 0 ) );
+            group3.setMatrix( osg.Matrix.makeTranslate( 0, 0, -5 ) );
+            group31.setMatrix( osg.Matrix.makeTranslate( 0, -5, 0 ) );
+            group32.setMatrix( osg.Matrix.makeTranslate( -5, 0, 0 ) );
+
+            var ground1 = osg.createTexturedBox( 0, 0, 0, 6, 5, 4 );
+            var ground2 = osg.createTexturedBoxGeometry( 0, 0, 0, 2, 2, 2 );
+            var ground3 = osg.createTexturedBox( 0, 0, 0, 1, 1, 1 );
+            var ground31 = osg.createTexturedBox( 0, 0, 0, 1, 1, 1 );
+            var ground32 = osg.createTexturedBox( 0, 0, 0, 1, 1, 1 );
+            var ground4 = osg.createTexturedBoxGeometry( 0, 0, 0, 2, 2, 2 );
+
+            var material2 = new osg.Material();
+            var material3 = new osg.Material();
+            var material4 = new osg.Material();
+            material2.setDiffuse( [ 0, 0, 1, 1 ] );
+            material3.setDiffuse( [ 0, 1, 0, 1 ] );
+            material4.setDiffuse( [ 1, 0, 0, 1 ] );
+            ground2.getOrCreateStateSet().setAttributeAndMode( material2 );
+            group3.getOrCreateStateSet().setAttributeAndMode( material3 );
+            group4.getOrCreateStateSet().setAttributeAndMode( material4 );
+
+            group1.addChild( ground1 );
+            group21.addChild( ground2 );
+            group22.addChild( ground2 );
+            group3.addChild( ground3 );
+            group3.addChild( group31 );
+            group3.addChild( group32 );
+            group31.addChild( ground31 );
+            group32.addChild( ground32 );
+            group4.addChild( ground4 );
+
+            root.addChild( group1 );
+            root.addChild( group21 );
+            root.addChild( group22 );
+            root.addChild( group3 );
+            root.addChild( group4 );
+
+            var visitor = new DebugVisitor;
+            root.accept( visitor );
+
+            this.createGraph( visitor );
+
+            return root;
+        },
+
+        run: function ( canvas ) {
+
+            var viewer;
+            viewer = new osgViewer.Viewer( canvas, {
+                antialias: true,
+                alpha: true
+            } );
+            viewer.init();
+
+            var rotate = new osg.MatrixTransform();
+            rotate.addChild( this.createScene() );
+
+            viewer.getCamera().setClearColor( [ 0.0, 0.0, 0.0, 0.0 ] );
+            viewer.setSceneData( rotate );
+            viewer.setupManipulator();
+            viewer.getManipulator().computeHomePosition();
+
+            viewer.run();
+
+            $( '.button' ).click( function () {
+                if ( $( 'svg' ).css( 'zIndex' ) === '-1' ) {
+                    $( '.button' ).text( 'Access to the scene' );
+                    $( 'svg' ).css( 'zIndex', '1' );
+                } else {
+                    $( '.button' ).text( 'Access to the graph' );
+                    $( 'svg' ).css( 'zIndex', '-1' );
+                }
             } );
         }
-    }
 
-    for ( i = 0; i < linkListSize; i++ ) {
-        g.addEdge( null, linkList[ i ].parentNode, linkList[ i ].childrenNode );
-    }
-
-    // Create the renderer
-    var renderer = new dagreD3.Renderer();
-
-    // Set up an SVG group so that we can translate the final graph.
-    var svg = d3.select( 'svg' ),
-        svgGroup = svg.append( 'g' );
-
-    // Set initial zoom to 75%
-    var initialScale = 0.75;
-    var oldZoom = renderer.zoom();
-    renderer.zoom( function ( graph, svg ) {
-        var zoom = oldZoom( graph, svg );
-
-        zoom.scale( initialScale ).event( svg );
-        return zoom;
-    } );
-
-    // Simple function to style the tooltip for the given node.
-    var styleTooltip = function ( name, description ) {
-        return '<p class="name">' + name + '</p><pre class="description">' + description + '</pre>';
     };
 
-    // Override drawNodes to set up the hover.
-    var oldDrawNodes = renderer.drawNodes();
-    renderer.drawNodes( function ( g, svg ) {
-        var svgNodes = oldDrawNodes( g, svg );
+    window.addEventListener( 'load', function () {
+        var example = new Example();
+        var canvas = $( '#View' )[ 0 ];
+        example.run( canvas );
+    }, true );
 
-        // Set the title on each of the nodes and use tipsy to display the tooltip on hover
-        svgNodes.attr( 'title', function ( d ) {
-            return styleTooltip( d, g.node( d ).description );
-        } );
-
-        return svgNodes;
-    } );
-
-    // Run the renderer. This is what draws the final graph.
-    var layout = renderer.run( g, svgGroup );
-
-    new SimpleTooltips({
-        selector: '.node'
-    });
-
-    $( '.node' ).click( function () {
-        var identifier = $( this ).attr( 'title' ).split( '<' )[ 1 ].split( '>' )[ 1 ];
-        if ( identifier.search( 'StateSet' ) === -1 ) {
-            window.activeNode = fullNodeList[ identifier ];
-            console.log( 'window.activeNode is set.' );
-            console.log( fullNodeList[ identifier ] );
-        } else {
-            var stateset = fullNodeList[ identifier.split( ' ' )[ 2 ] ].stateset;
-            window.activeStateset = stateset;
-            console.log( 'window.activeStateset is set.' );
-            console.log( stateset );
-
-        }
-
-    } );
-
-}
-
-function createScene() {
-
-    var root = new osg.Node();
-
-    var group1 = new osg.MatrixTransform();
-    var group21 = new osg.MatrixTransform();
-    var group22 = new osg.MatrixTransform();
-    var group3 = new osg.MatrixTransform();
-    var group31 = new osg.MatrixTransform();
-    var group32 = new osg.MatrixTransform();
-    var group4 = new osg.MatrixTransform();
-
-    group1.setMatrix( osg.Matrix.makeTranslate( +5, 10, -5 ) );
-    group21.setMatrix( osg.Matrix.makeTranslate( 0, 10, 0 ) );
-    group22.setMatrix( osg.Matrix.makeTranslate( 10, 0, 0 ) );
-    group3.setMatrix( osg.Matrix.makeTranslate( 0, 0, -5 ) );
-    group31.setMatrix( osg.Matrix.makeTranslate( 0, -5, 0 ) );
-    group32.setMatrix( osg.Matrix.makeTranslate( -5, 0, 0 ) );
-
-    var ground1 = osg.createTexturedBox( 0, 0, 0, 6, 5, 4 );
-    var ground2 = osg.createTexturedBoxGeometry( 0, 0, 0, 2, 2, 2 );
-    var ground3 = osg.createTexturedBox( 0, 0, 0, 1, 1, 1 );
-    var ground31 = osg.createTexturedBox( 0, 0, 0, 1, 1, 1 );
-    var ground32 = osg.createTexturedBox( 0, 0, 0, 1, 1, 1 );
-    var ground4 = osg.createTexturedBoxGeometry( 0, 0, 0, 2, 2, 2 );
-
-    var material2 = new osg.Material();
-    var material3 = new osg.Material();
-    var material4 = new osg.Material();
-    material2.setDiffuse( [ 0, 0, 1, 1 ] );
-    material3.setDiffuse( [ 0, 1, 0, 1 ] );
-    material4.setDiffuse( [ 1, 0, 0, 1 ] );
-    ground2.getOrCreateStateSet().setAttributeAndMode( material2 );
-    group3.getOrCreateStateSet().setAttributeAndMode( material3 );
-    group4.getOrCreateStateSet().setAttributeAndMode( material4 );
-
-    group1.addChild( ground1 );
-    group21.addChild( ground2 );
-    group22.addChild( ground2 );
-    group3.addChild( ground3 );
-    group3.addChild( group31 );
-    group3.addChild( group32 );
-    group31.addChild( ground31 );
-    group32.addChild( ground32 );
-    group4.addChild( ground4 );
-
-    root.addChild( group1 );
-    root.addChild( group21 );
-    root.addChild( group22 );
-    root.addChild( group3 );
-    root.addChild( group4 );
-
-    var visitor = new DebugVisitor;
-    root.accept( visitor );
-
-    createGraph();
-
-    return root;
-}
-
-window.addEventListener( 'load', main, true );
+} )();
