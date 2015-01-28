d4f36f73a21d8f74bec8fd673968205a98bb1bd4
Stéphane GINIER <stephane@sketchfab.com>
2015-01-28 16:44:56 +0100

    Adds rgbm and luv method to functions.glsl

--- sources/osgShader/node/functions.glsl
+++ sources/osgShader/node/functions.glsl
@@ -105,3 +105,25 @@ void normalizeNormalAndEyeVector( const in vec3 inputNormal, const in vec3 input
     normal = normalize( inputNormal );
     eye = normalize( -inputEye );
 }
+
+//http://graphicrants.blogspot.fr/2009/04/rgbm-color-encoding.html
+vec3 RGBMToRGB( const in vec4 rgba )
+{
+    const float maxRange = 8.0;
+    return rgba.rgb * maxRange * rgba.a;
+}
+
+const mat3 LUVInverse = mat3( 6.0013,    -2.700,   -1.7995,
+                              -1.332,    3.1029,   -5.7720,
+                              0.3007,    -1.088,    5.6268 );
+
+vec3 LUVToRGB( const in vec4 vLogLuv )
+{
+    float Le = vLogLuv.z * 255.0 + vLogLuv.w;
+    vec3 Xp_Y_XYZp;
+    Xp_Y_XYZp.y = exp2((Le - 127.0) / 2.0);
+    Xp_Y_XYZp.z = Xp_Y_XYZp.y / vLogLuv.y;
+    Xp_Y_XYZp.x = vLogLuv.x * Xp_Y_XYZp.z;
+    vec3 vRGB = LUVInverse * Xp_Y_XYZp;
+    return max(vRGB, 0.0);
+}
