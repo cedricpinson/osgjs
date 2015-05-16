330befae434b0f4a02f501354eee0e2d853ef9fa
Cedric Pinson <cedric@plopbyte.com>
2015-05-16 23:43:22 +0200

    WIP continue refactoring

--- sources/osgAnimation/Interpolator.js
+++ sources/osgAnimation/Interpolator.js
@@ -1,12 +1,20 @@
 define( [
-    'osg/Quat'
-], function ( Quat ) {
+
+], function () {
 
     var Vec3CopyKeyFrame = function( i, keys, result ) {
         var index = i;
         result[ 0 ] = keys[index++];
         result[ 1 ] = keys[index++];
         result[ 2 ] = keys[index++];
+    };
+
+    var Vec4CopyKeyFrame = function( i, keys, result ) {
+        var index = i;
+        result[ 0 ] = keys[index++];
+        result[ 1 ] = keys[index++];
+        result[ 2 ] = keys[index++];
+        result[ 3 ] = keys[index++];
     };
 
     var Vec3LerpInterpolator = function ( t, channelInstance ) {
@@ -54,232 +62,96 @@ define( [
     };
 
 
+    var QuatLerpInterpolator = function ( t, channelInstance ) {
 
+        var channel = channelInstance.channel;
+        var value = channelInstance.value;
+        var start = channel.start;
+        var end = channel.end;
+        var keys = channel.keys;
+        var times = channel.times;
 
-    /**
-     *  Interpolator provide interpolation function to sampler
-     */
-    var Vec3LerpInterpolatorOld = function ( keys, t, result ) {
-        var keyStart;
-        var startTime;
-        var keyEnd = keys[ keys.length - 1 ];
-        var endTime = keyEnd.t;
-        if ( t >= endTime ) {
-            result.key = 0;
-            result.value[ 0 ] = keyEnd[ 0 ];
-            result.value[ 1 ] = keyEnd[ 1 ];
-            result.value[ 2 ] = keyEnd[ 2 ];
+        if ( t >= end ) {
+            channelInstance.key = 0;
+            Vec4CopyKeyFrame( keys.length-4, keys, value );
             return;
-        } else {
-            keyStart = keys[ 0 ];
-            startTime = keyStart.t;
 
-            if ( t <= startTime ) {
-                result.key = 0;
-                result.value[ 0 ] = keyStart[ 0 ];
-                result.value[ 1 ] = keyStart[ 1 ];
-                result.value[ 2 ] = keyStart[ 2 ];
-                return;
-            }
+        } else if ( t <= start ) {
+            channelInstance.key = 0;
+            Vec4CopyKeyFrame( 0, keys, value );
+            return;
         }
 
-        var i1 = result.key;
-        while ( keys[ i1 + 1 ].t < t ) {
-            i1++;
-        }
-        var i2 = i1 + 1;
+        var i1 = channelInstance.key;
+        while ( times[ i1 + 1 ] < t ) i1++;
 
-        var t1 = keys[ i1 ].t;
-        var x1 = keys[ i1 ][ 0 ];
-        var y1 = keys[ i1 ][ 1 ];
-        var z1 = keys[ i1 ][ 2 ];
 
-        var t2 = keys[ i2 ].t;
-        var x2 = keys[ i2 ][ 0 ];
-        var y2 = keys[ i2 ][ 1 ];
-        var z2 = keys[ i2 ][ 2 ];
+        var t1 = times[ i1 ];
+        var t2 = times[ i1 + 1 ];
+
+        var index = i1 * 4;
+        var x1 = keys[ index++ ];
+        var y1 = keys[ index++ ];
+        var z1 = keys[ index++ ];
+        var w1 = keys[ index++ ];
+
+        var x2 = keys[ index++ ];
+        var y2 = keys[ index++ ];
+        var z2 = keys[ index++ ];
+        var w2 = keys[ index++ ];
 
         var r = ( t - t1 ) / ( t2 - t1 );
 
-        result.value[ 0 ] = x1 + ( x2 - x1 ) * r;
-        result.value[ 1 ] = y1 + ( y2 - y1 ) * r;
-        result.value[ 2 ] = z1 + ( z2 - z1 ) * r;
-        result.key = i1;
+        value[ 0 ] = x1 + ( x2 - x1 ) * r;
+        value[ 1 ] = y1 + ( y2 - y1 ) * r;
+        value[ 2 ] = z1 + ( z2 - z1 ) * r;
+        value[ 3 ] = w1 + ( w2 - w1 ) * r;
+        channelInstance.key = i1;
     };
 
-    var QuatLerpInterpolator = function ( keys, t, result ) {
-        var keyStart;
-        var startTime;
-        var keyEnd = keys[ keys.length - 1 ];
-        var endTime = keyEnd.t;
-        if ( t >= endTime ) {
-            result.key = 0;
-            result.value[ 0 ] = keyEnd[ 0 ];
-            result.value[ 1 ] = keyEnd[ 1 ];
-            result.value[ 2 ] = keyEnd[ 2 ];
-            result.value[ 3 ] = keyEnd[ 3 ];
+
+    var FloatLerpInterpolator = function ( t, channelInstance ) {
+
+        var channel = channelInstance.channel;
+        var value = channelInstance.value;
+        var start = channel.start;
+        var end = channel.end;
+        var keys = channel.keys;
+        var times = channel.times;
+
+        if ( t >= end ) {
+            channelInstance.key = 0;
+            channelInstance.value = keys[ keys.length-1 ] ;
             return;
-        } else {
-            keyStart = keys[ 0 ];
-            startTime = keyStart.t;
 
-            if ( t <= startTime ) {
-                result.key = 0;
-                result.value[ 0 ] = keyStart[ 0 ];
-                result.value[ 1 ] = keyStart[ 1 ];
-                result.value[ 2 ] = keyStart[ 2 ];
-                result.value[ 3 ] = keyStart[ 3 ];
-                return;
-            }
+        } else if ( t <= start ) {
+            channelInstance.key = 0;
+            channelInstance.value = keys[ 0 ] ;
+            return;
         }
 
-        var i1 = result.key;
-        while ( keys[ i1 + 1 ].t < t ) {
-            i1++;
-        }
-        var i2 = i1 + 1;
+        var i1 = channelInstance.key;
+        while ( times[ i1 + 1 ] < t ) i1++;
 
-        var t1 = keys[ i1 ].t;
-        var x1 = keys[ i1 ][ 0 ];
-        var y1 = keys[ i1 ][ 1 ];
-        var z1 = keys[ i1 ][ 2 ];
-        var w1 = keys[ i1 ][ 3 ];
 
-        var t2 = keys[ i2 ].t;
-        var x2 = keys[ i2 ][ 0 ];
-        var y2 = keys[ i2 ][ 1 ];
-        var z2 = keys[ i2 ][ 2 ];
-        var w2 = keys[ i2 ][ 3 ];
+        var t1 = times[ i1 ];
+        var t2 = times[ i1 + 1 ];
+
+        var index = i1 * 3;
+        var x1 = keys[ index++ ];
+
+        var x2 = keys[ index++ ];
 
         var r = ( t - t1 ) / ( t2 - t1 );
 
-        result.value[ 0 ] = x1 + ( x2 - x1 ) * r;
-        result.value[ 1 ] = y1 + ( y2 - y1 ) * r;
-        result.value[ 2 ] = z1 + ( z2 - z1 ) * r;
-        result.value[ 3 ] = w1 + ( w2 - w1 ) * r;
-        result.key = i1;
-    };
-
-    var QuatSlerpInterpolator = function ( keys, t, result ) {
-        var keyStart;
-        var startTime;
-        var keyEnd = keys[ keys.length - 1 ];
-        var endTime = keyEnd.t;
-        if ( t >= endTime ) {
-            result.key = 0;
-            result.value[ 0 ] = keyEnd[ 0 ];
-            result.value[ 1 ] = keyEnd[ 1 ];
-            result.value[ 2 ] = keyEnd[ 2 ];
-            result.value[ 3 ] = keyEnd[ 3 ];
-            return;
-        } else {
-            keyStart = keys[ 0 ];
-            startTime = keyStart.t;
-
-            if ( t <= startTime ) {
-                result.key = 0;
-                result.value[ 0 ] = keyStart[ 0 ];
-                result.value[ 1 ] = keyStart[ 1 ];
-                result.value[ 2 ] = keyStart[ 2 ];
-                result.value[ 3 ] = keyStart[ 3 ];
-                return;
-            }
-        }
-
-        var i1 = result.key;
-        while ( keys[ i1 + 1 ].t < t ) {
-            i1++;
-        }
-        var i2 = i1 + 1;
-
-        var t1 = keys[ i1 ].t;
-        var t2 = keys[ i2 ].t;
-        var r = ( t - t1 ) / ( t2 - t1 );
-
-        Quat.slerp( r, keys[ i1 ], keys[ i2 ], result.value );
-        result.key = i1;
-    };
-
-    /**
-     *  Interpolator provide interpolation function to sampler
-     */
-    var FloatLerpInterpolator = function ( keys, t, result ) {
-        var keyStart;
-        var startTime;
-        var keyEnd = keys[ keys.length - 1 ];
-        var endTime = keyEnd.t;
-        if ( t >= endTime ) {
-            result.key = 0;
-            result.value = keyEnd[ 0 ];
-            return;
-        } else {
-            keyStart = keys[ 0 ];
-            startTime = keyStart.t;
-
-            if ( t <= startTime ) {
-                result.key = 0;
-                result.value = keyStart[ 0 ];
-                return;
-            }
-        }
-
-        var i1 = result.key;
-        while ( keys[ i1 + 1 ].t < t ) {
-            i1++;
-        }
-        var i2 = i1 + 1;
-
-        var t1 = keys[ i1 ].t;
-        var x1 = keys[ i1 ][ 0 ];
-
-        var t2 = keys[ i2 ].t;
-        var x2 = keys[ i2 ][ 0 ];
-
-        var r = ( t - t1 ) / ( t2 - t1 );
-        result.value = x1 + ( x2 - x1 ) * r;
-        result.key = i1;
-    };
-
-    /**
-     *  Interpolator provide interpolation function to sampler
-     */
-    var FloatStepInterpolator = function ( keys, t, result ) {
-        var keyStart;
-        var startTime;
-        var keyEnd = keys[ keys.length - 1 ];
-        var endTime = keyEnd.t;
-        if ( t >= endTime ) {
-            result.key = 0;
-            result.value = keyEnd[ 0 ];
-            return;
-        } else {
-            keyStart = keys[ 0 ];
-            startTime = keyStart.t;
-
-            if ( t <= startTime ) {
-                result.key = 0;
-                result.value = keyStart[ 0 ];
-                return;
-            }
-        }
-
-        var i1 = result.key;
-        while ( keys[ i1 + 1 ].t < t ) {
-            i1++;
-        }
-        //var i2 = i1 + 1;
-
-        //var t1 = keys[ i1 ].t;
-        var x1 = keys[ i1 ][ 0 ];
-        result.value = x1;
-        result.key = i1;
+        value = x1 + ( x2 - x1 ) * r;
+        channelInstance.key = i1;
+        channelInstance.value = value;
     };
 
     return {
         Vec3LerpInterpolator: Vec3LerpInterpolator,
         QuatLerpInterpolator: QuatLerpInterpolator,
-        QuatSlerpInterpolator: QuatSlerpInterpolator,
-        FloatLerpInterpolator: FloatLerpInterpolator,
-        FloatStepInterpolator: FloatStepInterpolator
+        FloatLerpInterpolator: FloatLerpInterpolator
     };
 } );
--- tests/osgAnimation/Interpolator.js
+++ tests/osgAnimation/Interpolator.js
@@ -15,15 +15,7 @@ define( [
 
         QUnit.test( 'Vec3LerpInterpolator', function () {
 
-            var keys = {
-                keys: [
-                    1, 1, 1,
-                    0, 0, 0,
-                    3, 3, 3
-                ],
-                times: [ 0, 1, 2 ]
-            };
-
+            var keys = mockup.createVec3Keyframes();
             var timeArray = [ -1,
                               3,
                               0.5,
@@ -33,7 +25,7 @@ define( [
 
             var i;
             for ( i = 0; i < timeArray.length; i++) {
-                var c = Channel.createActiveVec3Channel( keys );
+                var c = Channel.createInstanceVec3Channel( keys );
                 channels.push( c );
             }
 
@@ -66,7 +58,7 @@ define( [
                 times: [ 0 ]
             };
 
-            var channelOneKey = Channel.createActiveVec3Channel( keys2 );
+            var channelOneKey = Channel.createInstanceVec3Channel( keys2 );
             result = channelOneKey;
 
             Interpolator.Vec3LerpInterpolator( 1, channelOneKey );
@@ -76,45 +68,42 @@ define( [
         } );
 
         QUnit.test( 'FloatLerpInterpolator', function () {
-            var keys = [];
-            keys.push( Keyframe.createFloatKeyframe( 0, 1 ) );
-            keys.push( Keyframe.createFloatKeyframe( 1, 0 ) );
-            keys.push( Keyframe.createFloatKeyframe( 2, 3 ) );
 
-            var result = {
-                'value': 0,
-                'key': 0
-            };
+            var keys = mockup.createFloatKeyframes();
 
-            Interpolator.FloatLerpInterpolator( keys, -1, result );
+            var timeArray = [ -1,
+                              3,
+                              0.5,
+                              1.5 ];
+
+            var channels = [];
+
+            var i;
+            for ( i = 0; i < timeArray.length; i++) {
+                var c = Channel.createInstanceFloatChannel( keys );
+                channels.push( c );
+            }
+
+            for ( i = 0; i < timeArray.length; i++)
+                Interpolator.FloatLerpInterpolator( timeArray[i], channels[i] );
+
+            var result;
+
+            result = channels[0];
             ok( mockup.checkNear( result.value, 1 ), 'Check value when time < first key' );
             ok( result.key === 0, 'Check key when time < first key' );
 
-            Interpolator.FloatLerpInterpolator( keys, 3, result );
+            result = channels[1];
             ok( mockup.checkNear( result.value, 3 ), 'Check value when time > last key' );
             ok( result.key === 0, 'Check key when time > last key' );
 
-            Interpolator.FloatLerpInterpolator( keys, 0.5, result );
+            result = channels[2];
             ok( mockup.checkNear( result.value, 0.5 ), 'Check value when time == 0.5' );
             ok( result.key === 0, 'Check key when time == 0.5' );
 
-            Interpolator.FloatLerpInterpolator( keys, 1.5, result );
+            result = channels[3];
             ok( mockup.checkNear( result.value, 1.5 ), 'Check value when time == 1.5' );
             ok( result.key === 1, 'Check key when time == 1.5' );
-
-            // with 2 keys only
-            keys = keys.slice( 1 );
-            result.key = 0;
-            Interpolator.FloatLerpInterpolator( keys, 1.5, result );
-            ok( mockup.checkNear( result.value, 1.5 ), 'Check value when time == 1.5 with 2 keyframes' );
-            ok( result.key === 0, 'Check key when time == 1.5 with 2 keyframes' );
-
-            // with 1 key only
-            keys = keys.slice( 1 );
-            result.key = 0;
-            Interpolator.FloatLerpInterpolator( keys, 2.0, result );
-            ok( mockup.checkNear( result.value, 3.0 ), 'Check value when time == 2.0 with 1 keyframe' );
-            ok( result.key === 0, 'Check key when time == 2.0 with 1 keyframe' );
 
         } );
     };
