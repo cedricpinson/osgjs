759557d9c35caa83ea549d75732ca10933fb4a00
Stéphane GINIER <stephane@sketchfab.com>
2015-06-18 11:25:19 +0200

    Removes clamp matrix warning

--- sources/osgViewer/Renderer.js
+++ sources/osgViewer/Renderer.js
@@ -92,101 +92,93 @@ define( [
             this.draw();
         },
 
-        cull: ( function () {
+        cull: function () {
 
-            var projectionMatrixTmp = Matrix.create();
-            var viewMatrixTmp = Matrix.create();
+            var camera = this.getCamera();
+            var view = camera.getView();
 
-            return function () {
+            this._cullVisitor.setFrameStamp( this._frameStamp );
 
-                var camera = this.getCamera();
-                var view = camera.getView();
-
-                this._cullVisitor.setFrameStamp( this._frameStamp );
-
-                // It should be done in RenderStage
-                this._cullVisitor.setCamera( this.getCamera() );
+            // It should be done in RenderStage
+            this._cullVisitor.setCamera( this.getCamera() );
 
 
-                // this part of code should be called for each view
-                // right now, we dont support multi view
-                this._stateGraph.clean();
-                this._renderStage.reset();
+            // this part of code should be called for each view
+            // right now, we dont support multi view
+            this._stateGraph.clean();
+            this._renderStage.reset();
 
-                this._cullVisitor.reset();
-                this._cullVisitor.setStateGraph( this._stateGraph );
-                this._cullVisitor.setRenderStage( this._renderStage );
+            this._cullVisitor.reset();
+            this._cullVisitor.setStateGraph( this._stateGraph );
+            this._cullVisitor.setRenderStage( this._renderStage );
 
-                this._cullVisitor.pushStateSet( camera.getStateSet() );
+            this._cullVisitor.pushStateSet( camera.getStateSet() );
 
-                // save cullSettings
-                var previousCullsettings = new CullSettings();
-                previousCullsettings.setCullSettings( this._cullVisitor );
-                this._cullVisitor.setCullSettings( camera );
-                if ( previousCullsettings.getSettingSourceOverrider() === this._cullVisitor && previousCullsettings.getEnableFrustumCulling() ) {
-                    this._cullVisitor.setEnableFrustumCulling( true );
+            // save cullSettings
+            var previousCullsettings = new CullSettings();
+            previousCullsettings.setCullSettings( this._cullVisitor );
+            this._cullVisitor.setCullSettings( camera );
+            if ( previousCullsettings.getSettingSourceOverrider() === this._cullVisitor && previousCullsettings.getEnableFrustumCulling() ) {
+                this._cullVisitor.setEnableFrustumCulling( true );
+            }
+
+            this._cullVisitor.pushModelViewMatrix( camera.getViewMatrix() );
+
+            this._cullVisitor.pushProjectionMatrix( camera.getProjectionMatrix() );
+
+            // update bound
+            camera.getBound();
+
+            var light = view.getLight();
+            var View = require( 'osgViewer/View' );
+
+            if ( light ) {
+
+                switch ( view.getLightingMode() ) {
+
+                case View.LightingMode.HEADLIGHT:
+                    this._cullVisitor.addPositionedAttribute( null, light );
+                    break;
+
+                case View.LightingMode.SKY_LIGHT:
+                    this._cullVisitor.addPositionedAttribute( camera.getViewMatrix(), light );
+                    break;
+
+                default:
+                    break;
                 }
+            }
 
-                this._cullVisitor.pushModelViewMatrix( Matrix.copy( camera.getViewMatrix(), viewMatrixTmp ) );
-
-                this._cullVisitor.pushProjectionMatrix( Matrix.copy( camera.getProjectionMatrix(), projectionMatrixTmp ) );
-
-                // update bound
-                camera.getBound();
-
-                var light = view.getLight();
-                var View = require( 'osgViewer/View' );
-
-                if ( light ) {
-
-                    switch ( view.getLightingMode() ) {
-
-                    case View.LightingMode.HEADLIGHT:
-                        this._cullVisitor.addPositionedAttribute( null, light );
-                        break;
-
-                    case View.LightingMode.SKY_LIGHT:
-                        this._cullVisitor.addPositionedAttribute( camera.getViewMatrix(), light );
-                        break;
-
-                    default:
-                        break;
-                    }
-                }
-
-                this._cullVisitor.pushViewport( camera.getViewport() );
+            this._cullVisitor.pushViewport( camera.getViewport() );
 
 
 
-                this._renderStage.setClearDepth( camera.getClearDepth() );
-                this._renderStage.setClearColor( camera.getClearColor() );
-                this._renderStage.setClearMask( camera.getClearMask() );
-                this._renderStage.setViewport( camera.getViewport() );
+            this._renderStage.setClearDepth( camera.getClearDepth() );
+            this._renderStage.setClearColor( camera.getClearColor() );
+            this._renderStage.setClearMask( camera.getClearMask() );
+            this._renderStage.setViewport( camera.getViewport() );
 
-                // pass de dbpager to the cullvisitor, so plod's can do the requests
-                this._cullVisitor.setDatabaseRequestHandler( this._camera.getView().getDatabasePager() );
-                // dont add camera on the stack just traverse it
-                this._cullVisitor.handleCullCallbacksAndTraverse( camera );
+            // pass de dbpager to the cullvisitor, so plod's can do the requests
+            this._cullVisitor.setDatabaseRequestHandler( this._camera.getView().getDatabasePager() );
+            // dont add camera on the stack just traverse it
+            this._cullVisitor.handleCullCallbacksAndTraverse( camera );
 
-                // fix projection matrix if camera has near/far auto compute
-                this._cullVisitor.popModelViewMatrix();
-                this._cullVisitor.popProjectionMatrix();
+            // fix projection matrix if camera has near/far auto compute
+            this._cullVisitor.popModelViewMatrix();
+            this._cullVisitor.popProjectionMatrix();
 
 
-                // store complete frustum
-                camera.setNearFar( this._cullVisitor._computedNear, this._cullVisitor._computedFar );
-                // Update near/far in the camera projection matrix
-                Matrix.clampProjectionMatrix( camera.getProjectionMatrix(), this._cullVisitor._computedNear, this._cullVisitor._computedFar, this._cullVisitor._nearFarRatio );
-                // restore previous state of the camera
-                this._cullVisitor.setCullSettings( previousCullsettings );
+            // store complete frustum
+            camera.setNearFar( this._cullVisitor._computedNear, this._cullVisitor._computedFar );
+            // restore previous state of the camera
+            this._cullVisitor.setCullSettings( previousCullsettings );
 
-                this._cullVisitor.popViewport();
-                this._cullVisitor.popStateSet();
+            this._cullVisitor.popViewport();
+            this._cullVisitor.popStateSet();
 
-                this._renderStage.sort();
+            this._renderStage.sort();
 
-            };
-        } )(),
+        },
 
         draw: function () {
 
