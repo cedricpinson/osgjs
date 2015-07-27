cce67ae369a76be11771c35a5a01e3b9bc2dd568
Guillaume Faucillion <guillaume@sketchfab.com>
2015-07-27 12:29:12 +0200

    Adds dirtyBound() for update

--- sources/osgAnimation/UpdateMatrixTransform.js
+++ sources/osgAnimation/UpdateMatrixTransform.js
@@ -17,6 +17,8 @@ define( [
         this._stackedTransforms = [];
 
         this._matrix = Matrix.create();
+
+        this._dirty = false;
     };
 
 
@@ -27,6 +29,7 @@ define( [
         },
 
         computeChannels: function () {
+            this._dirty = true;
             var matrix = this._matrix;
             Matrix.makeIdentity( matrix );
             var transforms = this._stackedTransforms;
@@ -39,6 +42,10 @@ define( [
 
         update: function ( node /*, nv */ ) {
             Matrix.copy( this._matrix, node.getMatrix() );
+            if ( this._dirty ) {
+                node.dirtyBound();
+                this._dirty = false;
+            }
             return true;
         }
 
