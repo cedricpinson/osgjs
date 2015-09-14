096edc8d63e1267ae0bb5d74bd0c83a69edc96b9
Stéphane GINIER <stephane@sketchfab.com>
2015-09-14 18:30:08 +0200

    Updates bone size name variable

--- sources/osgAnimation/AnimationAttribute.js
+++ sources/osgAnimation/AnimationAttribute.js
@@ -12,31 +12,33 @@ define( [
      * @class AnimationAttribute
      * @inherits StateAttribute
      */
-    var AnimationAttribute = function ( disable, boneSize ) {
+    var AnimationAttribute = function ( disable, boneUniformSize ) {
         StateAttribute.call( this );
         this._enable = !disable;
-        this._boneSize = boneSize; // optional, if it's not provided, it will fall back to the maximum bone size
+        // optional, if it's not provided, it will fall back to the maximum bone uniform size
+        // boneUniformSize represents the number of vec4 (uniform) used in the shader for all the bones
+        this._boneUniformSize = boneUniformSize;
     };
 
     AnimationAttribute.uniforms = {};
-    AnimationAttribute.maxBoneSize = 1;
-    AnimationAttribute.maxBoneAllowed = Infinity; // can be overriden by application specific limit on startup (typically gl limit)
+    AnimationAttribute.maxBoneUniformSize = 1;
+    AnimationAttribute.maxBoneUniformAllowed = Infinity; // can be overriden by application specific limit on startup (typically gl limit)
 
     AnimationAttribute.prototype = MACROUTILS.objectLibraryClass( MACROUTILS.objectInherit( StateAttribute.prototype, {
 
         attributeType: 'AnimationAttribute',
         cloneType: function () {
-            return new AnimationAttribute( true, this._boneSize );
+            return new AnimationAttribute( true, this._boneUniformSize );
         },
-        setBoneSize: function ( boneSize ) {
-            this._boneSize = boneSize;
+        setBoneUniformSize: function ( boneUniformSize ) {
+            this._boneUniformSize = boneUniformSize;
         },
-        getBoneSize: function () {
-            return this._boneSize !== undefined ? this._boneSize : AnimationAttribute.maxBoneSize;
+        getBoneUniformSize: function () {
+            return this._boneUniformSize !== undefined ? this._boneUniformSize : AnimationAttribute.maxBoneUniformSize;
         },
 
         getTypeMember: function () {
-            return this.attributeType + '_' + this.getBoneSize();
+            return this.attributeType + '_' + this.getBoneUniformSize();
         },
 
         getOrCreateUniforms: function () {
@@ -56,9 +58,9 @@ define( [
         setMatrixPalette: function ( matrixPalette ) {
             this._matrixPalette = matrixPalette;
             // update max bone size
-            if ( this._boneSize === undefined ) {
-                AnimationAttribute.maxBoneSize = Math.max( AnimationAttribute.maxBoneSize, matrixPalette.length / 4 );
-                AnimationAttribute.maxBoneSize = Math.min( AnimationAttribute.maxBoneAllowed, AnimationAttribute.maxBoneSize );
+            if ( this._boneUniformSize === undefined ) {
+                AnimationAttribute.maxBoneUniformSize = Math.max( AnimationAttribute.maxBoneUniformSize, matrixPalette.length / 3 );
+                AnimationAttribute.maxBoneUniformSize = Math.min( AnimationAttribute.maxBoneUniformAllowed, AnimationAttribute.maxBoneUniformSize );
             }
         },
         getMatrixPalette: function () {
