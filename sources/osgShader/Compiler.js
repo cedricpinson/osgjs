4de4638c7d79be460a1d4e7ac702b7960a0435db
Jordi Torres <jordi.torres@mirage-tech.com>
2016-02-15 15:58:42 +0100

    Fix billboard on matrix scaled nodes

--- sources/osgShader/Compiler.js
+++ sources/osgShader/Compiler.js
@@ -1350,13 +1350,12 @@ Compiler.prototype = {
 
     declareVertexTransformBillboard: function ( glPosition ) {
         this.getOrCreateInputPosition();
-        var billboard = [ '%glPosition = %ProjectionMatrix * ( vec4( %Vertex, 1.0 ) + vec4( %ModelViewMatrix[ 3 ].xyz, 0.0 ) );', ];
-        this.getNode( 'InlineCode' ).code( billboard.join( '\n' ) ).inputs( {
-            ModelViewMatrix: this.getOrCreateUniform( 'mat4', 'ModelViewMatrix' ),
+        this.getNode( 'Billboard' ).inputs( {
             Vertex: this.getOrCreateAttribute( 'vec3', 'Vertex' ),
+            ModelViewMatrix: this.getOrCreateUniform( 'mat4', 'ModelViewMatrix' ),
             ProjectionMatrix: this.getOrCreateUniform( 'mat4', 'ProjectionMatrix' )
         } ).outputs( {
-            glPosition: glPosition
+            vec: glPosition
         } );
     },
 
@@ -1391,7 +1390,7 @@ Compiler.prototype = {
     // Transform Position into NDC
     // but keep intermediary result
     // FragEye which is in Camera/Eye space
-    // (most light computation are in eye space)
+    // (most light computation is in eye space)
     // (better precision, particulary if camera is far from World 0.0.0)
     declareTransformWithEyeSpace: function ( glPosition ) {
 
--- sources/osgShader/shaderLib.js
+++ sources/osgShader/shaderLib.js
@@ -5,12 +5,13 @@ var skinning = require( 'osgShader/node/skinning.glsl' );
 var textures = require( 'osgShader/node/textures.glsl' );
 var colorEncode = require( 'osgShader/node/colorEncode.glsl' );
 var noise = require( 'osgShader/node/noise.glsl' );
-
+var billboard = require( 'osgShader/node/billboard.glsl' );
 module.exports = {
     'functions.glsl': functions,
     'lights.glsl': lights,
     'skinning.glsl': skinning,
     'textures.glsl': textures,
     'colorEncode.glsl': colorEncode,
-    'noise.glsl': noise
+    'noise.glsl': noise,
+    'billboard.glsl': billboard
 };
