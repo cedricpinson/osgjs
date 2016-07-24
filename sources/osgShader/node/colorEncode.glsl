1e8c0d330422ef5455086d37aa6e873af6be02dd
Jordi Torres <jtorresfabra@gmail.com>
2016-07-24 11:51:39 +0200

    Fixed error in color encode

--- sources/osgShader/node/colorEncode.glsl
+++ sources/osgShader/node/colorEncode.glsl
@@ -1,10 +1,9 @@
 
 float decodeFloatRGBA( vec4 rgba ) {
-    return dot( rgba, vec4(1.0, 1.0/255.0, 1.0/65025.0, 1.0/160581375.0) );
-}
+    return dot( rgba, vec4(1.0, 1.0/255.0, 1.0/65025.0, 1.0/16581375.0) );
 
 vec4 encodeFloatRGBA( float v ) {
-    vec4 enc = vec4(1.0, 255.0, 65025.0, 160581375.0) * v;
+    vec4 enc = vec4(1.0, 255.0, 65025.0, 16581375.0) * v;
     enc = fract(enc);
     enc -= enc.yzww * vec4(1.0/255.0,1.0/255.0,1.0/255.0,0.0);
     return enc;
