25e71bf48901ddfbe2f00f1b5180c479318a98bc
Stéphane GINIER <stephane@sketchfab.com>
2016-08-01 18:08:32 +0200

    Fixes color encode float

--- sources/osgShader/node/colorEncode.glsl
+++ sources/osgShader/node/colorEncode.glsl
@@ -1,6 +1,7 @@
 
 float decodeFloatRGBA( vec4 rgba ) {
     return dot( rgba, vec4(1.0, 1.0/255.0, 1.0/65025.0, 1.0/16581375.0) );
+}
 
 vec4 encodeFloatRGBA( float v ) {
     vec4 enc = vec4(1.0, 255.0, 65025.0, 16581375.0) * v;
