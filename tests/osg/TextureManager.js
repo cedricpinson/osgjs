3ef05344e6a80af70f93c9509541c407d5a9de2c
Cedric Pinson <cedric@plopbyte.com>
2016-08-24 19:08:15 +0200

    Fixes texture manager flush test

--- tests/osg/TextureManager.js
+++ tests/osg/TextureManager.js
@@ -86,7 +86,7 @@ module.exports = function () {
         assert.equal( tm._textureSetMap[ '3553640810241024' ].getUsedTextureObjects().length, 0, 'check used 1024x1024 empty after release' );
 
         // flush TO's in 0.001 seconds
-        tm.flushDeletedTextureObjects( gl, 0.0001 );
+        tm.flushDeletedTextureObjects( gl, 0.00001 );
         // There is no time to flush all the released texture objects
         assert.isOk( tm._textureSetMap[ '3553640810241024' ].getOrphanedTextureObjects().length > 0, 'check orphan 1024x1024 delete not empty' );
         // flush all TO's
