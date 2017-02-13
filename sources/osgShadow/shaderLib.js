310bb6d6dfc9ff5de7302cecfda572b08d3b5433
Kuranes <tuan.kuranes@gmail.com>
2017-02-13 10:27:08 +0000

    [#D3D-2902] Removes other shadow algo
    Simplifies code maintaining

--- sources/osgShadow/shaderLib.js
+++ sources/osgShadow/shaderLib.js
@@ -4,14 +4,8 @@ var shadowsReceive = require( 'osgShadow/shaders/shadowsReceive.glsl' );
 var shadowsReceiveMain = require( 'osgShadow/shaders/shadowsReceiveMain.glsl' );
 var shadowLinearSoft = require( 'osgShadow/shaders/shadowLinearSoft.glsl' );
 var pcf = require( 'osgShadow/shaders/pcf.glsl' );
-var bandPCF = require( 'osgShadow/shaders/bandPCF.glsl' );
 var tapPCF = require( 'osgShadow/shaders/tapPCF.glsl' );
 var hash = require( 'osgShadow/shaders/hash.glsl' );
-var arrayPoisson = require( 'osgShadow/shaders/arrayPoisson.glsl' );
-var poissonPCF = require( 'osgShadow/shaders/poissonPCF.glsl' );
-var esm = require( 'osgShadow/shaders/esm.glsl' );
-var vsm = require( 'osgShadow/shaders/vsm.glsl' );
-var evsm = require( 'osgShadow/shaders/evsm.glsl' );
 
 module.exports = {
     'shadowsCastFrag.glsl': shadowsCastFrag,
@@ -19,12 +13,6 @@ module.exports = {
     'shadowsReceiveMain.glsl': shadowsReceiveMain,
     'shadowLinearSoft.glsl': shadowLinearSoft,
     'pcf.glsl': pcf,
-    'bandPCF.glsl': bandPCF,
     'tapPCF.glsl': tapPCF,
-    'hash.glsl': hash,
-    'arrayPoisson.glsl': arrayPoisson,
-    'poissonPCF.glsl': poissonPCF,
-    'esm.glsl': esm,
-    'vsm.glsl': vsm,
-    'evsm.glsl': evsm
+    'hash.glsl': hash
 };
