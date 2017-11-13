79e2f31bb9a3d77a6deed49e0ec01f70d70f50d4
Cedric Pinson <trigrou@trigrou.com>
2017-11-13 21:44:26 +0100

    Fixes script to build website

--- website/build-website.sh
+++ website/build-website.sh
@@ -7,17 +7,37 @@ rm -rf "${build_dir}/*"
 git clone ../ --branch master --single-branch "${build_dir}/osg-clone"
 cd "${build_dir}/osg-clone"
 
+# tell gh-pages to do ignores vendors or node_modules
+touch "${build_dir}/.nojekyll"
+
 npm install
 grunt sync
-mkdir -p ${build_dir}/node_modules/mocha
+
+mkdir -p "${build_dir}/node_modules/mocha"
 cp node_modules/mocha/mocha.* "${build_dir}/node_modules/mocha"
+
+mkdir -p "${build_dir}/node_modules/hammerjs/"
 cp node_modules/hammerjs/hammer.min.js "${build_dir}/node_modules/hammerjs/hammer.min.js"
+
+mkdir -p "${build_dir}/node_modules/bluebird/js/browser/"
 cp node_modules/bluebird/js/browser/bluebird.min.js "${build_dir}/node_modules/bluebird/js/browser/bluebird.min.js"
+
+mkdir -p "${build_dir}/node_modules/jquery/dist/"
 cp node_modules/jquery/dist/jquery.min.js "${build_dir}/node_modules/jquery/dist/jquery.min.js"
+
+mkdir -p "${build_dir}/node_modules/dat.gui/build/"
 cp node_modules/dat.gui/build/dat.gui.min.js "${build_dir}/node_modules/dat.gui/build/dat.gui.min.js"
+
+mkdir -p "${build_dir}/node_modules/hammerjs/"
 cp node_modules/hammerjs/hammer.min.js "${build_dir}/node_modules/hammerjs/hammer.min.js"
+
+mkdir -p "${build_dir}/node_modules/zlibjs/bin/"
 cp node_modules/zlibjs/bin/gunzip.min.js "${build_dir}/node_modules/zlibjs/bin/gunzip.min.js"
+
+mkdir -p "${build_dir}/node_modules/d3/build/"
 cp node_modules/d3/build/d3.min.js "${build_dir}/node_modules/d3/build/d3.min.js"
+
+mkdir -p "${build_dir}/node_modules/dagre/dist/"
 cp node_modules/dagre/dist/dagre.min.js "${build_dir}/node_modules/dagre/dist/dagre.min.js"
 
 grunt build docs
